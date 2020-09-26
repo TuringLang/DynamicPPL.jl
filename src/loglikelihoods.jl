4f2b1469c1abdb9ca79ad949dcba4bc6afc5439d
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2020-09-26 16:26:30 +0100

    added docstring

--- src/loglikelihoods.jl
+++ src/loglikelihoods.jl
@@ -59,6 +59,57 @@ function tilde_observe(
     return left
 end
 
+"""
+    loglikelihoods(model::Model, chain::Chains)
+
+Runs `model` on each sample in `chain` returning an array of arrays with
+the i-th element inner arrays corresponding to the the likelihood of the i-th
+observation for that particular sample in `chain`.
+
+# Notes
+Say `y` is a `Vector` of `n` i.i.d. `Normal(μ, σ)` variables, with `μ` and `σ`
+both being `<:Real`. Then the observe statements can be implemented in two ways:
+```julia
+for i in eachindex(y)
+    y[i] ~ Normal(μ, σ)
+end
+```
+or
+```julia
+y ~ MvNormal(fill(μ, n), fill(σ, n))
+```
+Unfortunately, just by looking at the latter statement, it's impossible to tell whether or
+not this is one *single* observation which is `n` dimensional OR if we have *multiple*
+1-dimensional observations. Therefore, `loglikelihoods` will only work with the first
+example.
+
+# Examples
+```julia-repl
+julia> using DynamicPPL, Turing
+
+julia> @model function demo(xs, y)
+           s ~ InverseGamma(2, 3)
+           m ~ Normal(0, √s)
+           for i in eachindex(xs)
+               xs[i] ~ Normal(m, √s)
+           end
+
+           y ~ Normal(m, √s)
+       end
+demo (generic function with 1 method)
+
+julia> model = demo(randn(3), randn());
+
+julia> chain = sample(model, MH(), 10);
+
+julia> DynamicPPL.loglikelihoods(model, chain)
+Dict{String,Array{Float64,1}} with 4 entries:
+  "xs[3]" => [-1.02616, -1.26931, -1.05003, -5.05458, -1.33825, -1.02904, -1.23761, -1.30128, -1.04872, -2.03716]
+  "xs[1]" => [-2.08205, -2.51387, -3.03175, -2.5981, -2.31322, -2.62284, -2.70874, -1.18617, -1.36281, -4.39839]
+  "xs[2]" => [-2.20604, -2.63495, -3.22802, -2.48785, -2.40941, -2.78791, -2.85013, -1.24081, -1.46019, -4.59025]
+  "y"     => [-1.36627, -1.21964, -1.03342, -7.46617, -1.3234, -1.14536, -1.14781, -2.48912, -2.23705, -1.26267]
+```
+"""
 function loglikelihoods(model::Model, chain)
     # Get the data by executing the model once
     spl = LikelihoodSampler()
