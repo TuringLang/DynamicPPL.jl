f5c87f27c93ead00f29345ab2ef8a2bee459b968
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2020-09-28 16:27:10 +0100

    updated docstring

--- src/loglikelihoods.jl
+++ src/loglikelihoods.jl
@@ -92,11 +92,14 @@ end
 
 
 """
-    elementwise_loglikelihoods(model::Model, chain::Chains)
+    elementwise_loglikelihoods(model::Model, chain::Chains, keytype = String)
 
 Runs `model` on each sample in `chain` returning a `Dict{String, Matrix{Float64}}`
 with keys corresponding to symbols of the observations, and values being matrices
 of shape `(num_chains, num_samples)`.
+
+`keytype` specifies what the type of the keys used in the returned `Dict` are.
+Currently, only `String` and `VarName` are supported.
 
 # Notes
 Say `y` is a `Vector` of `n` i.i.d. `Normal(μ, σ)` variables, with `μ` and `σ`
@@ -111,10 +114,10 @@ or
 ```julia
 y ~ MvNormal(fill(μ, n), fill(σ, n))
 ```
-Unfortunately, just by looking at the latter statement, it's impossible to tell whether or
-not this is one *single* observation which is `n` dimensional OR if we have *multiple*
-1-dimensional observations. Therefore, `loglikelihoods` will only work with the first
-example.
+Unfortunately, just by looking at the latter statement, it's impossible to tell 
+whether or not this is one *single* observation which is `n` dimensional OR if we
+have *multiple* 1-dimensional observations. Therefore, `loglikelihoods` will only
+work with the first example.
 
 # Examples
 ```julia-repl
@@ -135,12 +138,26 @@ julia> model = demo(randn(3), randn());
 
 julia> chain = sample(model, MH(), 10);
 
-julia> DynamicPPL.elementwise_loglikelihoods(model, chain)
-Dict{String,Array{Float64,1}} with 4 entries:
-  "xs[3]" => [-1.02616, -1.26931, -1.05003, -5.05458, -1.33825, -1.02904, -1.23761, -1.30128, -1.04872, -2.03716]
-  "xs[1]" => [-2.08205, -2.51387, -3.03175, -2.5981, -2.31322, -2.62284, -2.70874, -1.18617, -1.36281, -4.39839]
-  "xs[2]" => [-2.20604, -2.63495, -3.22802, -2.48785, -2.40941, -2.78791, -2.85013, -1.24081, -1.46019, -4.59025]
-  "y"     => [-1.36627, -1.21964, -1.03342, -7.46617, -1.3234, -1.14536, -1.14781, -2.48912, -2.23705, -1.26267]
+julia> elementwise_loglikelihoods(model, chain)
+Dict{String,Array{Float64,2}} with 4 entries:
+  "xs[3]" => [-1.42862; -2.67573; … ; -1.66251; -1.66251]
+  "xs[1]" => [-1.42932; -2.68123; … ; -1.66333; -1.66333]
+  "xs[2]" => [-1.6724; -0.861339; … ; -1.62359; -1.62359]
+  "y"     => [-1.51265; -0.914129; … ; -1.5499; -1.5499]
+
+julia> elementwise_loglikelihoods(model, chain, String)
+Dict{String,Array{Float64,2}} with 4 entries:
+  "xs[3]" => [-1.42862; -2.67573; … ; -1.66251; -1.66251]
+  "xs[1]" => [-1.42932; -2.68123; … ; -1.66333; -1.66333]
+  "xs[2]" => [-1.6724; -0.861339; … ; -1.62359; -1.62359]
+  "y"     => [-1.51265; -0.914129; … ; -1.5499; -1.5499]
+
+julia> elementwise_loglikelihoods(model, chain, VarName)
+Dict{VarName,Array{Float64,2}} with 4 entries:
+  xs[2] => [-1.6724; -0.861339; … ; -1.62359; -1.62359]
+  y     => [-1.51265; -0.914129; … ; -1.5499; -1.5499]
+  xs[1] => [-1.42932; -2.68123; … ; -1.66333; -1.66333]
+  xs[3] => [-1.42862; -2.67573; … ; -1.66251; -1.66251]
 ```
 """
 function elementwise_loglikelihoods(
