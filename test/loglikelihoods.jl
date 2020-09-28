308b2ca4eb2a15e31171e06be3b195b397a6c38c
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2020-09-28 17:42:29 +0100

    fixed tests

--- test/loglikelihoods.jl
+++ test/loglikelihoods.jl
@@ -22,8 +22,7 @@ using .Turing
     @test haskey(var_to_likelihoods, "y")
 
     for chain_idx in MCMCChains.chains(chain)
-
-        for (i, (s, m)) in enumerate(zip(chain[:s], chain[:m]))
+        for (i, (s, m)) in enumerate(zip(chain[:, :s, chain_idx], chain[:, :m, chain_idx]))
             @test logpdf(Normal(m, √s), xs[1]) == var_to_likelihoods["xs[1]"][i, chain_idx]
             @test logpdf(Normal(m, √s), xs[2]) == var_to_likelihoods["xs[2]"][i, chain_idx]
             @test logpdf(Normal(m, √s), xs[3]) == var_to_likelihoods["xs[3]"][i, chain_idx]
