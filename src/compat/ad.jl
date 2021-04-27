aaab9bb679f7cba2ae66a90733ce797480b6bee9
David Widmann <david.widmann@it.uu.se>
2021-04-27 18:36:24 +0000

    Fix Zygote issue with `dot_observe` (#236)
    This PR fixes https://github.com/TuringLang/Turing.jl/issues/1595.
    
    It is an alternative to https://github.com/TuringLang/DynamicPPL.jl/pull/235 that does not require us to rewrite the primal less efficiently which would affect regular execution and other AD backends.
    
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- src/compat/ad.jl
+++ src/compat/ad.jl
@@ -12,3 +12,17 @@ ChainRulesCore.@non_differentiable updategid!(
     vn::VarName,
     spl::Sampler,
 )
+
+# https://github.com/TuringLang/Turing.jl/issues/1595
+ZygoteRules.@adjoint function dot_observe(
+    spl::Union{SampleFromPrior, SampleFromUniform},
+    dists::AbstractArray{<:Distribution},
+    value::AbstractArray,
+    vi,
+)
+    function dot_observe_fallback(spl, dists, value, vi)
+        increment_num_produce!(vi)
+        return sum(map(Distributions.loglikelihood, dists, value))
+    end
+    return ZygoteRules.pullback(__context__, dot_observe_fallback, spl, dists, value, vi)
+end
