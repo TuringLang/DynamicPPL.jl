e32b59fc0c51d89b68fff66fa790da34f646b7b6
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2021-05-18 20:36:20 +0200

    initial stuff

---
+++ test/benchmarks/utils.jl
@@ -0,0 +1,49 @@
+using DynamicPPL
+using BenchmarkTools
+
+import Weave
+import Markdown
+
+function time_model_def(model_def, args...)
+    return @time model_def(args...)
+end
+
+function benchmark_untyped_varinfo!(suite, m)
+    vi = VarInfo()
+    # Populate.
+    m(vi)
+    # Evaluate.
+    suite["evaluation_untyped"] = @benchmarkable $m($vi)
+    return suite
+end
+
+function benchmark_typed_varinfo!(suite, m)
+    # Populate.
+    vi = VarInfo(m)
+    # Evaluate.
+    suite["evaluation_typed"] = @benchmarkable $m($vi)
+    return suite
+end
+
+function typed_code(m, vi = VarInfo(m))
+    rng = DynamicPPL.Random.MersenneTwister(42);
+    spl = DynamicPPL.SampleFromPrior()
+    ctx = DynamicPPL.DefaultContext()
+
+    return Main.@code_typed m.f(rng, m, vi, spl, ctx, m.args...)
+end
+
+function make_suite(m)
+    suite = BenchmarkGroup()
+    benchmark_untyped_varinfo!(suite, m)
+    benchmark_typed_varinfo!(suite, m)
+
+    return suite
+end
+
+function weave_child(indoc; mod, args, kwargs...)
+    doc = Weave.WeaveDoc(indoc, nothing)
+    doc = Weave.run_doc(doc, doctype = "github", mod = mod, args = args, kwargs...)
+    rendered = Weave.render_doc(doc)
+    return display(Markdown.parse(rendered))
+end
