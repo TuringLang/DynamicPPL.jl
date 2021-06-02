4d2cc143d84171c73c40712994f5d54b1754dc64
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2021-06-03 00:58:25 +0100

    formatting

--- test/contexts.jl
+++ test/contexts.jl
@@ -1,12 +1,16 @@
 @testset "contexts.jl" begin
     @testset "PrefixContext" begin
-        ctx = @inferred Prefix{:a}(Prefix{:b}(Prefix{:c}(Prefix{:d}(Prefix{:e}(Prefix{:f}())))))
+        ctx = @inferred PrefixContext{:a}(
+            PrefixContext{:b}(
+                PrefixContext{:c}(PrefixContext{:d}(PrefixContext{:e}(PrefixContext{:f}())))
+            ),
+        )
         vn = VarName{:x}()
         vn_prefixed = @inferred DynamicPPL.prefix(ctx, vn)
         @test DynamicPPL.getsym(vn_prefixed) == Symbol("a.b.c.d.e.f.x")
         @test vn_prefixed.indices === vn.indices
 
-        vn = VarName{:x}((1, ))
+        vn = VarName{:x}((1,))
         vn_prefixed = @inferred DynamicPPL.prefix(ctx, vn)
         @test DynamicPPL.getsym(vn_prefixed) == Symbol("a.b.c.d.e.f.x")
         @test vn_prefixed.indices === vn.indices
