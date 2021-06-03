01435dde36c3c128521381c5a9e28654db3ee852
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2021-06-03 01:10:44 +0100

    okay actually fixed tests

--- test/contexts.jl
+++ test/contexts.jl
@@ -12,11 +12,11 @@
         vn = VarName{:x}()
         vn_prefixed = @inferred DynamicPPL.prefix(ctx, vn)
         @test DynamicPPL.getsym(vn_prefixed) == Symbol("a.b.c.d.e.f.x")
-        @test vn_prefixed.indices === vn.indices
+        @test vn_prefixed.indexing === vn.indexing
 
         vn = VarName{:x}((1,))
         vn_prefixed = @inferred DynamicPPL.prefix(ctx, vn)
         @test DynamicPPL.getsym(vn_prefixed) == Symbol("a.b.c.d.e.f.x")
-        @test vn_prefixed.indices === vn.indices
+        @test vn_prefixed.indexing === vn.indexing
     end
 end
