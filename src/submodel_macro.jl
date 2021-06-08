70957d27c66a3a1ce7b3ac751e68709531b5e548
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2021-06-08 08:03:50 +0100

    updated submodel macro

--- src/submodel_macro.jl
+++ src/submodel_macro.jl
@@ -1,10 +1,8 @@
 macro submodel(expr)
     return quote
         _evaluate(
-            $(esc(:__rng__)),
             $(esc(expr)),
             $(esc(:__varinfo__)),
-            $(esc(:__sampler__)),
             $(esc(:__context__)),
         )
     end
@@ -13,10 +11,8 @@ end
 macro submodel(prefix, expr)
     return quote
         _evaluate(
-            $(esc(:__rng__)),
             $(esc(expr)),
             $(esc(:__varinfo__)),
-            $(esc(:__sampler__)),
             PrefixContext{$(esc(Meta.quot(prefix)))}($(esc(:__context__))),
         )
     end
