1d9bc373cdb23fd70cca8cae26bc54af9a0d157e
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2021-06-20 02:01:34 +0100

    update submodel-macro

--- src/submodel_macro.jl
+++ src/submodel_macro.jl
@@ -1,15 +1,33 @@
 macro submodel(expr)
-    return quote
-        _evaluate($(esc(expr)), $(esc(:__varinfo__)), $(esc(:__context__)))
+    args_tilde = getargs_tilde(expr)
+    return if args_tilde === nothing
+        # In this case we only want to get the `__varinfo__`.
+        quote
+            $(esc(:_)), $(esc(:__varinfo__)) = _evaluate_with_varinfo($(esc(expr)), $(esc(:__varinfo__)), $(esc(:__context__)))
+        end
+    else
+        # Here we also want the return-variable.
+        L, R = args_tilde
+        quote
+            $(esc(L)), $(esc(:__varinfo__)) = _evaluate_with_varinfo($(esc(R)), $(esc(:__varinfo__)), $(esc(:__context__)))
+        end
     end
 end
 
 macro submodel(prefix, expr)
-    return quote
-        _evaluate(
-            $(esc(expr)),
-            $(esc(:__varinfo__)),
-            PrefixContext{$(esc(Meta.quot(prefix)))}($(esc(:__context__))),
-        )
+    ctx = :(PrefixContext{$(esc(Meta.quot(prefix)))}($(esc(:__context__))))
+
+    args_tilde = getargs_tilde(expr)
+    return if args_tilde === nothing
+        # In this case we only want to get the `__varinfo__`.
+        quote
+            $(esc(:_)), $(esc(:__varinfo__)) = _evaluate_with_varinfo($(esc(expr)), $(esc(:__varinfo__)), $(ctx))
+        end
+    else
+        # Here we also want the return-variable.
+        L, R = args_tilde
+        quote
+            $(esc(L)), $(esc(:__varinfo__)) = _evaluate_with_varinfo($(esc(R)), $(esc(:__varinfo__)), $(ctx))
+        end
     end
 end
