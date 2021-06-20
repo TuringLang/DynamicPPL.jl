14b94956bdb14587b7321267dba20d98d54b0171
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2021-06-20 02:47:56 +0100

    simplifed submodel macro a bit

--- src/submodel_macro.jl
+++ src/submodel_macro.jl
@@ -9,28 +9,15 @@ prefixed by `prefix`. This is useful if you have variables of same names in
 several models used together.
 """
 macro submodel(expr)
-    args_tilde = getargs_tilde(expr)
-    return if args_tilde === nothing
-        # In this case we only want to get the `__varinfo__`.
-        quote
-            $(esc(:_)), $(esc(:__varinfo__)) = _evaluate_with_varinfo(
-                $(esc(expr)), $(esc(:__varinfo__)), $(esc(:__context__))
-            )
-        end
-    else
-        # Here we also want the return-variable.
-        L, R = args_tilde
-        quote
-            $(esc(L)), $(esc(:__varinfo__)) = _evaluate_with_varinfo(
-                $(esc(R)), $(esc(:__varinfo__)), $(esc(:__context__))
-            )
-        end
-    end
+    return submodel(expr)
 end
 
 macro submodel(prefix, expr)
     ctx = :(PrefixContext{$(esc(Meta.quot(prefix)))}($(esc(:__context__))))
+    return submodel(expr, ctx)
+end
 
+function submodel(expr, ctx = esc(:__context__))
     args_tilde = getargs_tilde(expr)
     return if args_tilde === nothing
         # In this case we only want to get the `__varinfo__`.
