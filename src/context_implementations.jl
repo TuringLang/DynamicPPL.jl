5f3ba935c1c0d96df88846ab57f1791f066168c4
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2021-07-27 01:25:46 +0100

    use view in _getindex

--- src/context_implementations.jl
+++ src/context_implementations.jl
@@ -14,7 +14,7 @@ alg_str(spl::Sampler) = string(nameof(typeof(spl.alg)))
 require_gradient(spl::Sampler) = false
 require_particles(spl::Sampler) = false
 
-_getindex(x, inds::Tuple) = _getindex(x[first(inds)...], Base.tail(inds))
+_getindex(x, inds::Tuple) = _getindex(view(x, first(inds)...), Base.tail(inds))
 _getindex(x, inds::Tuple{}) = x
 
 # assume
