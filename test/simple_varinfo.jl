04dab05f8838b2c4addb9ed7d03952503919e69b
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2021-11-17 04:00:07 +0000

    added more tests

--- test/simple_varinfo.jl
+++ test/simple_varinfo.jl
@@ -13,6 +13,13 @@
             @test !haskey(svi, @varname(m[2]))
             @test svi[@varname(m)][1] == svi[@varname(m[1])]
 
+            svi = SimpleVarInfo(; m=(a=[1.0],))
+            @test haskey(svi, @varname(m))
+            @test haskey(svi, @varname(m.a))
+            @test haskey(svi, @varname(m.a[1]))
+            @test !haskey(svi, @varname(m.a[2]))
+            @test !haskey(svi, @varname(m.a.b))
+
             svi = SimpleVarInfo{Float32}(; m=1.0)
             @test getlogp(svi) isa Float32
 
@@ -32,6 +39,22 @@
             @test haskey(svi, @varname(m[1]))
             @test !haskey(svi, @varname(m[2]))
             @test svi[@varname(m)][1] == svi[@varname(m[1])]
+
+            svi = SimpleVarInfo(Dict(@varname(m) => (a=[1.0],)))
+            @test haskey(svi, @varname(m))
+            @test haskey(svi, @varname(m.a))
+            @test haskey(svi, @varname(m.a[1]))
+            @test !haskey(svi, @varname(m.a[2]))
+            @test !haskey(svi, @varname(m.a.b))
+
+            svi = SimpleVarInfo(Dict(@varname(m.a) => [1.0]))
+            # Now we only have a variable `m.a` which is subsumed by `m`,
+            # but we can't guarantee that we have the "entire" `m`.
+            @test !haskey(svi, @varname(m))
+            @test haskey(svi, @varname(m.a))
+            @test haskey(svi, @varname(m.a[1]))
+            @test !haskey(svi, @varname(m.a[2]))
+            @test !haskey(svi, @varname(m.a.b))
         end
     end
 
@@ -39,32 +62,67 @@
         # We might need to pre-allocate for the variable `m`, so we need
         # to see whether this is the case.
         m = model().m
-        svi = if m isa AbstractArray
+        svi_nt = if m isa AbstractArray
             SimpleVarInfo((m=similar(m),))
         else
             SimpleVarInfo()
         end
+        svi_dict = SimpleVarInfo(VarInfo(model), Dict)
 
-        # Sample a new varinfo!
-        _, svi_new = DynamicPPL.evaluate!!(model, svi, SamplingContext())
-        # Type of realization for `m` should be unchanged.
-        @test typeof(svi_new[@varname(m)]) === typeof(m)
-        # Realization for `m` should be different wp. 1.
-        @test svi_new[@varname(m)] != m
-        # Logjoint should be non-zero wp. 1.
-        @test getlogp(svi_new) != 0
+        @testset "$(nameof(typeof(svi.values)))" for svi in (svi_nt, svi_dict)
+            # Random seed is set in each `@testset`, so we need to sample
+            # a new realization for `m` here.
+            m = model().m
 
-        # Evaluation.
-        m_eval = if m isa AbstractArray
-            randn!(similar(m))
-        else
-            randn(eltype(m))
+            ### Sampling ###
+            # Sample a new varinfo!
+            _, svi_new = DynamicPPL.evaluate!!(model, svi, SamplingContext())
+
+            # If the `m[1]` varname doesn't exist, this is a univariate model.
+            # TODO: Find a better way of dealing with this that is not dependent
+            # on knowledge of internals of `model`.
+            isunivariate = !haskey(svi_new, @varname(m[1]))
+
+            # Realization for `m` should be different wp. 1.
+            if isunivariate
+                @test svi_new[@varname(m)] != m
+            else
+                @test svi_new[@varname(m[1])] != m[1]
+                @test svi_new[@varname(m[2])] != m[2]
+            end
+            # Logjoint should be non-zero wp. 1.
+            @test getlogp(svi_new) != 0
+
+            ### Evaluation ###
+            # Sample some random testing values.
+            m_eval = if m isa AbstractArray
+                randn!(similar(m))
+            else
+                randn(eltype(m))
+            end
+
+            # Update the realizations in `svi_new`.
+            svi_eval = if isunivariate
+                DynamicPPL.setindex!!(svi_new, m_eval, @varname(m))
+            else
+                DynamicPPL.setindex!!(svi_new, m_eval, [@varname(m[1]), @varname(m[2])])
+            end
+            # Reset the logp field.
+            svi_eval = DynamicPPL.resetlogp!!(svi_eval)
+
+            # Compute `logjoint` using the varinfo.
+            logπ = logjoint(model, svi_eval)
+            # Extract the parameters from `svi_eval`.
+            m_vi = if isunivariate
+                svi_eval[@varname(m)]
+            else
+                svi_eval[[@varname(m[1]), @varname(m[2])]]
+            end
+            # These should not have changed.
+            @test m_vi == m_eval
+            # Compute the true `logjoint` and compare.
+            logπ_true = DynamicPPL.TestUtils.logjoint_true(model, m_vi)
+            @test logπ ≈ logπ_true
         end
-        svi_eval = @set svi_new.values.m = m_eval
-        svi_eval = DynamicPPL.resetlogp!!(svi_eval)
-
-        logπ = logjoint(model, svi_eval)
-        logπ_true = DynamicPPL.TestUtils.logjoint_true(model, svi_eval.values.m)
-        @test logπ ≈ logπ_true
     end
 end
