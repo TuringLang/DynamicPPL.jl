274aebb5bac013e7d1fcff4d1b12323870b106d9
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2021-12-14 21:38:21 +0000

    added docfilter for nested LoadError

--- docs/make.jl
+++ docs/make.jl
@@ -18,6 +18,9 @@ makedocs(;
         r"(Array{.+,\s?1}|Vector{.+})",
         # Older versions will show "Array{...,2}" instead of "Matrix{...}".
         r"(Array{.+,\s?2}|Matrix{.+})",
+        # Errors from macros sometimes result in `LoadError: LoadError:`
+        # rather than `LoadError:`, depending on Julia version.
+        r"ERROR: LoadError: (LoadError:\s)?+"
     ],
 )
 
