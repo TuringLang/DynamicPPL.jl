8f8acc4b3b42a73c50fc161dea39b2f82564352b
Carlos Parada <paradac@carleton.edu>
2022-02-16 14:21:01 -0800

    Update src/contexts.jl
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- src/contexts.jl
+++ src/contexts.jl
@@ -121,9 +121,9 @@ setleafcontext(::IsLeaf, ::IsLeaf, left, right) = right
 # Contexts
 """
     SamplingContext(
-            rng=Random.GLOBAL_RNG, 
-            sampler::AbstractSampler=SampleFromPrior(), 
-            context::AbstractContext=DefaultContext(),
+            [rng::Random.AbstractRNG=Random.GLOBAL_RNG],
+            [sampler::AbstractSampler=SampleFromPrior()],
+            [context::AbstractContext=DefaultContext()],
     )
 
 Create a context that allows you to sample parameters with the `sampler` when running the model.
