dd109134bda4574ba1a9f4ec2cda8a44f40c2947
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2022-07-01 17:50:16 +0100

    just fix potential bug in getindex for VarInfo

--- src/varinfo.jl
+++ src/varinfo.jl
@@ -944,9 +944,7 @@ function getindex(vi::AbstractVarInfo, vn::VarName, dist::Distribution)
     return maybe_invlink(vi, vn, dist, val)
 end
 function getindex(vi::AbstractVarInfo, vns::Vector{<:VarName})
-    # NOTE(torfjelde): Using `getdist(vi, first(vns))` won't be correct in cases
-    # such as `x .~ [Normal(), Exponential()]`.
-    return getindex(vi, vns, getdist(vi, first(vns)))
+    return getindex.((vi,), vns, getdist.((vi,), vns))
 end
 function getindex(vi::AbstractVarInfo, vns::Vector{<:VarName}, dist::Distribution)
     @assert haskey(vi, vns[1]) "[DynamicPPL] attempted to replay unexisting variables in VarInfo"
