075ac8190d7cd890774f35180b0b8d4994711278
YongchaoHuang <34540771+YongchaoHuang@users.noreply.github.com>
2023-01-11 14:46:49 +0000

    modified src/logp.jl; added test/logp.jl

--- src/logp.jl
+++ src/logp.jl
@@ -1,23 +1,23 @@
 # functions for evaluating logp: log posterior, log likelihood and log prior
 
-## 1. evaluate log prior at sample parameter positions
-function logprior(model_instance::Model, chain::AbstractMCMC.AbstractChains)
-    """
-    This function evaluates the `log prior` for chain.
-    -- Inputs 
-        model_instance: the probabilistic model instance;
-        chain: an MCMC chain.
-    -- Outputs
-        lls: a Vector of log prior values.
-    """
-    varinfo = VarInfo(model_instance) # extract variables info from the model
+#### 1. logprior ####
+"""
+This function evaluates the `log prior` for chain.
+-- Inputs 
+    model_instance: the probabilistic model instance;
+    chain: an MCMC chain.
+-- Outputs
+    lls: a Vector of log prior values.
+"""
+function chain_logprior(model_instance::Model, chain::AbstractMCMC.AbstractChains)
+    vi = VarInfo(model_instance) # extract variables info from the model
     lls = Matrix{Float64}(undef, size(chain, 1), size(chain, 3)) # initialize a matrix to store the evaluated log posterior
     for chain_idx in 1:size(chain, 3)
         for iteration_idx in 1:size(chain, 1)
             argvals_dict = OrderedDict(
                 vn => chain[iteration_idx, Symbol(vn), chain_idx] for
-                vn_parent in keys(varinfo) for
-                vn in DynamicPPL.TestUtils.varname_leaves(vn_parent, varinfo[vn_parent])
+                vn_parent in keys(vi) for
+                vn in DynamicPPL.TestUtils.varname_leaves(vn_parent, vi[vn_parent])
             )
             # Compute and store.
             lls[iteration_idx, chain_idx] = DynamicPPL.logprior(
@@ -28,26 +28,59 @@ function logprior(model_instance::Model, chain::AbstractMCMC.AbstractChains)
     return lls
 end
 
-## 2. evaluate log likelihood at sample parameter positions
-function Distributions.loglikelihood(
-    model_instance::Model, chain::AbstractMCMC.AbstractChains
-)
-    """
+"""
+    This function evaluates the `log prior` for chain.
+    -- Inputs 
+        model: the probabilistic model instance;
+        nt_array: an array of NamedTuple of sample parameter values.
+    -- Outputs
+        lls: a Vector of log prior values.
+"""
+function chain_logprior(model_instance::Model, nt_arr::Vector{NamedTuple})
+    lls = Array{Float64}(undef, size(nt_arr, 1)) # initialize a matrix to store the evaluated log posterior
+    for param_idx in 1:size(nt_arr, 1)
+        # Compute and store.
+        lls[param_idx] = DynamicPPL.logprior(model_instance, nt_arr[param_idx])
+    end
+    return lls
+end
+
+"""
+    This function evaluates the `log prior` for chain.
+    -- Inputs 
+        model: the probabilistic model instance;
+        nt_array: an array of NamedTuple of sample parameter values.
+    -- Outputs
+        lls: a Vector of log prior values.
+"""
+function chain_logprior(model_instance::Model, nt_arr::Vector{Any})
+    lls = Array{Float64}(undef, size(nt_arr, 1)) # initialize a matrix to store the evaluated log posterior
+    for param_idx in 1:size(nt_arr, 1)
+        # Compute and store.
+        lls[param_idx] = DynamicPPL.logprior(model_instance, nt_arr[param_idx])
+    end
+    return lls
+end
+
+
+#### 2. loglikelihood ####
+"""
     This function evaluates the `log likelihood` for chain.
     -- Inputs 
         model: the probabilistic model instance;
         chain: an MCMC chain 
     -- Outputs
         lls: a Vector of log likelihood values.
-    """
-    varinfo = VarInfo(model_instance) # extract variables info from the model
+"""
+function chain_loglikelihoods(model_instance::Model, chain::AbstractMCMC.AbstractChains)
+    vi = VarInfo(model_instance) # extract variables info from the model
     lls = Matrix{Float64}(undef, size(chain, 1), size(chain, 3)) # initialize a matrix to store the evaluated log posterior
     for chain_idx in 1:size(chain, 3)
         for iteration_idx in 1:size(chain, 1)
             argvals_dict = OrderedDict(
                 vn => chain[iteration_idx, Symbol(vn), chain_idx] for
-                vn_parent in keys(varinfo) for
-                vn in DynamicPPL.TestUtils.varname_leaves(vn_parent, varinfo[vn_parent])
+                vn_parent in keys(vi) for
+                vn in DynamicPPL.TestUtils.varname_leaves(vn_parent, vi[vn_parent])
             )
             # Compute and store.
             lls[iteration_idx, chain_idx] = Distributions.loglikelihood(
@@ -58,17 +91,52 @@ function Distributions.loglikelihood(
     return lls
 end
 
-## 3. evaluate log posterior at sample parameter positions
-function logjoint(model_instance::Model, chain::AbstractMCMC.AbstractChains)
-    """
-    This function evaluates the `log posterior` for chain.
-    -- Inputs 
-        model: the probabilistic model instance;
-        chain: an MCMC chain object.
-    -- Outputs
-        lls: a Vector of log posterior values.
-    """
-    varinfo = VarInfo(model_instance) # extract variables info from the model
+"""
+This function evaluates the `log likelihood` for chain.
+-- Inputs 
+    model: the probabilistic model instance;
+    nt_array: an array of NamedTuple of sample parameter values.
+-- Outputs
+    lls: a Vector of log likelihood values.
+"""
+function chain_loglikelihoods(model_instance::Model, nt_arr::Vector{NamedTuple})
+    lls = Array{Float64}(undef, size(nt_arr, 1)) # initialize a matrix to store the evaluated log posterior
+    for param_idx in 1:size(nt_arr, 1)
+        # Compute and store.
+        lls[param_idx] = Distributions.loglikelihood(model_instance, nt_arr[param_idx])
+    end
+    return lls
+end
+
+"""
+This function evaluates the `log likelihood` for chain.
+-- Inputs 
+    model: the probabilistic model instance;
+    nt_array: an array of NamedTuple of sample parameter values.
+-- Outputs
+    lls: a Vector of log likelihood values.
+"""
+function chain_loglikelihoods(model_instance::Model, nt_arr::Vector{Any})
+    lls = Array{Float64}(undef, size(nt_arr, 1)) # initialize a matrix to store the evaluated log posterior
+    for param_idx in 1:size(nt_arr, 1)
+        # Compute and store.
+        lls[param_idx] = Distributions.loglikelihood(model_instance, nt_arr[param_idx])
+    end
+    return lls
+end
+
+#### 3. logjoint ####
+"""
+This function evaluates the `log posterior` for chain.
+-- Inputs 
+    model: the probabilistic model instance;
+    chain: an MCMC chain object.
+-- Outputs
+    lls: a Vector of log posterior values.
+"""
+## evaluate log posterior at sample parameter positions
+function chain_logjoint(model_instance::Model, chain::AbstractMCMC.AbstractChains)
+    vi = VarInfo(model_instance) # extract variables info from the model
     lls = Matrix{Float64}(undef, size(chain, 1), size(chain, 3)) # initialize a matrix to store the evaluated log posterior
     for chain_idx in 1:size(chain, 3)
         for iteration_idx in 1:size(chain, 1)
@@ -76,8 +144,8 @@ function logjoint(model_instance::Model, chain::AbstractMCMC.AbstractChains)
             # TODO: This does not work for cases where the model has dynamic support, i.e. some of the iterations might have differently sized parameter space.
             argvals_dict = OrderedDict(
                 vn => chain[iteration_idx, Symbol(vn), chain_idx] for
-                vn_parent in keys(varinfo) for
-                vn in DynamicPPL.TestUtils.varname_leaves(vn_parent, varinfo[vn_parent])
+                vn_parent in keys(vi) for
+                vn in DynamicPPL.TestUtils.varname_leaves(vn_parent, vi[vn_parent])
             )
             # Compute and store.
             lls[iteration_idx, chain_idx] =
@@ -88,15 +156,34 @@ function logjoint(model_instance::Model, chain::AbstractMCMC.AbstractChains)
     return lls
 end
 
-function logjoint(model_instance::Model, nt_arr::Vector{NamedTuple})
-    """
-    This function evaluates the `log posterior` for chain.
-    -- Inputs 
-        model: the probabilistic model instance;
-        nt_array: an array of NamedTuple of sample parameter values.
-    -- Outputs
-        lls: a Vector of log posterior values.
-    """
+"""
+This function evaluates the `log posterior` for chain.
+-- Inputs 
+    model: the probabilistic model instance;
+    nt_array: an array of NamedTuple of sample parameter values.
+-- Outputs
+    lls: a Vector of log posterior values.
+"""
+function chain_logjoint(model_instance::Model, nt_arr::Vector{NamedTuple})
+    lls = Array{Float64}(undef, size(nt_arr, 1)) # initialize a matrix to store the evaluated log posterior
+    for param_idx in 1:size(nt_arr, 1)
+        # Compute and store.
+        lls[param_idx] =
+            Distributions.loglikelihood(model_instance, nt_arr[param_idx]) +
+            DynamicPPL.logprior(model_instance, nt_arr[param_idx])
+    end
+    return lls
+end
+
+"""
+This function evaluates the `log posterior` for chain.
+-- Inputs 
+    model: the probabilistic model instance;
+    nt_array: an array of NamedTuple of sample parameter values.
+-- Outputs
+    lls: a Vector of log posterior values.
+"""
+function chain_logjoint(model_instance::Model, nt_arr::Vector{Any})
     lls = Array{Float64}(undef, size(nt_arr, 1)) # initialize a matrix to store the evaluated log posterior
     for param_idx in 1:size(nt_arr, 1)
         # Compute and store.
--- test/logp.jl
+++ test/logp.jl
@@ -1,25 +1,40 @@
+@testset "logp.jl" begin
+    Test.@testset "$(m.f)" for m in DynamicPPL.TestUtils.DEMO_MODELS
+        # generate a chain of sample parameter values.
+        N = 100
+        chain = Vector(undef, N)
+        vi_vector = Dict()
+        logpriors_true = Vector(undef, N)
+        loglikelihoods_true = Vector(undef, N)
+        logposteriors_true = Vector(undef, N)
+        for i in 1:N
+            # generate samples and extrac vi
+            example_values = rand(NamedTuple, m)
+            print(example_values)
+            chain[i] = example_values
+            # append!(chain, [example_values])
+            # Instantiate a `VarInfo` with the example values.
+            vi = VarInfo(m)
+            for vn in DynamicPPL.TestUtils.varnames(m)
+                vi = DynamicPPL.setindex!!(vi, get(example_values, vn), vn)
+            end
+            vi_vector["$i"] = vi
 
-## Test 
-@model function demo_model(x)
-    s ~ InverseGamma(2, 3)
-    m ~ Normal(0, sqrt(s))
-    for i in 1:length(x)
-        x[i] ~ Normal(m, sqrt(s))
+            # calculate the true pointwise likelihood
+            logprior_true = DynamicPPL.TestUtils.logprior_true(m, example_values...)
+            logpriors_true[i] = logprior_true
+            loglikelihood_true = DynamicPPL.TestUtils.loglikelihood_true(m, example_values...)
+            loglikelihoods_true[i] = loglikelihood_true
+            logposterior_true = logprior_true + loglikelihood_true
+            logposteriors_true[i] = logposterior_true
+        end
+        # calculate the pointwise loglikelihoods for the whole chain using custom logprior.
+        logpriors_new = chain_logprior(m, chain)
+        loglikelihoods_new = chain_loglikelihoods(m, chain)
+        logposteriors_new = chain_logjoint(m, chain)
+        # compare the likelihoods
+        @test logpriors_new ≈ logpriors_true
+        @test loglikelihoods_new ≈ loglikelihoods_true
+        @test logposteriors_new ≈ logposteriors_true
     end
 end
-
-# generate data
-using StableRNGs, Turing
-rng = StableRNG(111)
-using Random, Distributions
-Random.seed!(111)
-x = rand(Normal(1.0, 1.0), 1000)
-
-# MCMC sampling 
-demo_model_instance = demo_model(x)
-chain = sample(demo_model_instance, NUTS(0.65), 5_00) # chain: 1st index is the iteration no, 3rd index is the chain no.
-
-# evaluate logp
-lls = logprior(demo_model_instance, chain)
-lls = loglikelihood(demo_model_instance, chain)
-lls = logjoint(demo_model_instance, chain)
