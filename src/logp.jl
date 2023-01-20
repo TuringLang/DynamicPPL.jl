e9e515f2feaf5d3fae066bb68f86982bd29de10e
YongchaoHuang <34540771+YongchaoHuang@users.noreply.github.com>
2023-01-20 19:37:35 +0000

    Update src/logp.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/logp.jl
+++ src/logp.jl
@@ -115,9 +115,7 @@ julia> logjoint(demo_model([1., 2.]), chain, 2);
 ```   
 """
 function logjoint(
-	model_instance::Model,
-	chain::AbstractMCMC.AbstractChains,
-	start_idx::Int = 1,
+    model_instance::Model, chain::AbstractMCMC.AbstractChains, start_idx::Int=1
 )
 	vi = VarInfo(model_instance) # extract variables info from the model
 	map(
