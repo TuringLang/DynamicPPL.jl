a364e3b1523ceb136cf488f8e2fb9034ad8f7831
YongchaoHuang <34540771+YongchaoHuang@users.noreply.github.com>
2023-01-22 14:02:38 +0000

    Update src/model.jl
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- src/model.jl
+++ src/model.jl
@@ -798,9 +798,7 @@ function Distributions.loglikelihood(
     model_instance::Model, chain::AbstractMCMC.AbstractChains, start_idx::Int=1
 )
     vi = VarInfo(model_instance) # extract variables info from the model
-    map(
-        Iterators.product(start_idx:size(chain, 1), 1:size(chain, 3))
-    ) do (iteration_idx, chain_idx)
+    map(Iterators.product(1:size(chain, 1), 1:size(chain, 3))) do (iteration_idx, chain_idx)
         argvals_dict = OrderedDict(
             vn => chain[iteration_idx, Symbol(vn), chain_idx] for vn_parent in keys(vi) for
             vn in DynamicPPL.TestUtils.varname_leaves(vn_parent, vi[vn_parent])
