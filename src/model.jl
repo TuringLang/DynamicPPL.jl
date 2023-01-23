13b9a7f9d0dd0c6689a6d918fd3e64b232a870b8
YongchaoHuang <34540771+YongchaoHuang@users.noreply.github.com>
2023-01-23 10:31:27 +0000

    Update src/model.jl
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- src/model.jl
+++ src/model.jl
@@ -740,7 +740,7 @@ function logprior(model::Model, chain::AbstractMCMC.AbstractChains)
     map(Iterators.product(1:size(chain, 1), 1:size(chain, 3))) do (iteration_idx, chain_idx)
         argvals_dict = OrderedDict(
             vn => chain[iteration_idx, Symbol(vn), chain_idx] for vn_parent in keys(vi) for
-            vn in DynamicPPL.TestUtils.varname_leaves(vn_parent, vi[vn_parent])
+            vn in TestUtils.varname_leaves(vn_parent, vi[vn_parent])
         )
         DynamicPPL.logprior(model_instance, argvals_dict)
     end
