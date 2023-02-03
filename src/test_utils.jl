4fe5eea2a20f1f33a787c7da65c3c83fb65b50f2
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2023-02-03 11:33:28 +0000

    remove mentioning of Exp and Identity in test_utils.jl

--- src/test_utils.jl
+++ src/test_utils.jl
@@ -686,7 +686,7 @@ Simple model for which [`default_transformation`](@ref) returns a [`StaticTransf
 end
 
 function DynamicPPL.default_transformation(::Model{typeof(demo_static_transformation)})
-    b = Bijectors.stack(Bijectors.Exp(), Bijectors.Identity())
+    b = Bijectors.stack(exp, identity)
     return DynamicPPL.StaticTransformation(b)
 end
 
