f5a8113b480fb1c15bdd818f467f65f14b454caf
harisorgn <organtzh@gmail.com>
2023-06-23 16:27:36 +0300

    test `LKJCholesky` for both `'U'` and `'L'`

--- test/lkj.jl
+++ test/lkj.jl
@@ -1,7 +1,9 @@
-using Bijectors: pd_from_upper
+using Bijectors: pd_from_upper, pd_from_lower
+
+pd_from_triangular(X::AbstractMatrix, uplo::Char) = uplo == 'U' ? pd_from_upper(X) : pd_from_lower(X)
 
 @model lkj_prior_demo() = x ~ LKJ(2, 1)
-@model lkj_chol_prior_demo() = x ~ LKJCholesky(2, 1, 'U')
+@model lkj_chol_prior_demo(uplo) = x ~ LKJCholesky(2, 1, uplo)
 
 # Same for both distributions
 target_mean = vec(Matrix{Float64}(I, 2, 2))
@@ -25,15 +27,15 @@ _lkj_atol = 0.05
     end
 end
 
-@testset "Sample from x ~ LKJCholesky(2, 1, $(chol_char))" for chol_char in ['L', 'U']
-    model = lkj_chol_prior_demo(chol_char)
+@testset "Sample from x ~ LKJCholesky(2, 1, $(uplo))" for uplo in ['U', 'L']
+    model = lkj_chol_prior_demo(uplo)
     # `SampleFromPrior` will sample in unconstrained space.
     @testset "SampleFromPrior" begin
         samples = sample(model, SampleFromPrior(), 1_000)
         # Build correlation matrix from factor
         corr_matrices = map(samples) do s
-            M = Float64.(reshape(s.metadata.vals, (2, 2)))
-            pd_from_upper(M)
+            M = float.(reshape(s.metadata.vals, (2, 2)))
+            pd_from_triangular(M, uplo)
         end
         @test vec(mean(corr_matrices)) ≈ target_mean atol = _lkj_atol
     end
@@ -44,7 +46,7 @@ end
         # Build correlation matrix from factor
         corr_matrices = map(samples) do s
             M = float.(reshape(s.metadata.vals, (2, 2)))
-            pd_from_upper(M)
+            pd_from_triangular(M, uplo)
         end
         @test vec(mean(corr_matrices)) ≈ target_mean atol = _lkj_atol
     end
