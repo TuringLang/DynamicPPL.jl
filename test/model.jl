50def53a467e251f842b9d6a14da875a35b734c4
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2023-07-17 16:58:38 +0100

    added missing posterior_mean and rand implementation, in simple checks to make sure these things do not go unnoticied

--- test/model.jl
+++ test/model.jl
@@ -185,4 +185,19 @@ end
             end
         end
     end
+
+    @testset "TestUtils" begin
+        @testset "$(model.f)" for model in DynamicPPL.TestUtils.DEMO_MODELS
+            x = rand(model)
+            # Ensure log-probability computations are implemented.
+            @test logprior(model, x) ≈ DynamicPPL.TestUtils.logprior_true(model, x...)
+            @test loglikelihood(model, x) ≈ DynamicPPL.TestUtils.loglikelihood_true(model, x...)
+            @test logjoint(model, x) ≈ DynamicPPL.TestUtils.logjoint_true(model, x...)
+            @test logjoint(model, x) != logprior_true_with_logabsdet_jacobian(model, x...)
+            # Ensure `varnames` is implemented.
+            @test all(keys(VarInfo(model)) .== keys(DynamicPPL.TestUtils.varnames(model)))
+            # Ensure `posterior_mean` is implemented.
+            @test DynamicPPL.TestUtils.posterior_mean(model) isa typeof(x)
+        end
+    end
 end
