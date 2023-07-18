c2a87aab6b34adf9270dd2157c93368e10332851
Xianda Sun <sunxdt@gmail.com>
2023-07-18 10:52:04 +0100

    Revert "use `Setfield.set` in `set!!` function, experiment"
    This reverts commit d7cf4ca3ab2441a15c2c78ec2a70848da676fa4f.

--- src/utils.jl
+++ src/utils.jl
@@ -332,14 +332,12 @@ collectmaybe(x::Base.AbstractSet) = collect(x)
 # BangBang.jl related #
 #######################
 function set!!(obj, lens::Setfield.Lens, value)
-    # lensmut = BangBang.prefermutation(lens)
-    # return Setfield.set(obj, lensmut, value)
-    return Setfield.set(obj, lens, value)
+    lensmut = BangBang.prefermutation(lens)
+    return Setfield.set(obj, lensmut, value)
 end
 function set!!(obj, vn::VarName{sym}, value) where {sym}
-    # lens = BangBang.prefermutation(Setfield.PropertyLens{sym}() ∘ AbstractPPL.getlens(vn))
-    # return Setfield.set(obj, lens, value)
-    return AbstractPPL.set(obj, vn, value)
+    lens = BangBang.prefermutation(Setfield.PropertyLens{sym}() ∘ AbstractPPL.getlens(vn))
+    return Setfield.set(obj, lens, value)
 end
 
 #############################
