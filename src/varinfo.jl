050e0998224b10690ab4bba17854010d4293b8a1
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2023-08-31 11:59:14 +0100

    added explicit invlink implementation for VarInfo

--- src/varinfo.jl
+++ src/varinfo.jl
@@ -899,6 +899,65 @@ function _inner_transform!(vi::VarInfo, vn::VarName, dist, f)
     return vi
 end
 
+function invlink(varinfo::UntypedVarInfo)
+    varinfo = deepcopy(varinfo)
+    return VarInfo(_invlink_metadata!(varinfo, varinfo.metadata), Base.Ref(getlogp(varinfo)), Ref(get_num_produce(varinfo)))
+end
+
+function invlink(varinfo::TypedVarInfo)
+    varinfo = deepcopy(varinfo)
+    md = map(Base.Fix1(_invlink_metadata!, varinfo), varinfo.metadata)
+    # TODO: Update logp, etc.
+    return VarInfo(md, Base.Ref(getlogp(varinfo)), Ref(get_num_produce(varinfo)))
+end
+
+function _invlink_metadata!(varinfo::VarInfo, metadata::Metadata)
+    vns = metadata.vns
+
+    # Construct the new transformed values, and keep track of their lengths.
+    lengths_new = Int[]
+    vals_new = mapreduce(vcat, vns) do vn
+        x = getval(varinfo, vn)
+        dist = getdist(varinfo, vn)
+        # Return early if we're not transforming.
+        !istrans(varinfo, vn) && vectorize(dist, x)
+        # Transform to constrained space.
+        f = invlink_transform(dist)
+        y, logjac = with_logabsdet_jacobian_and_reconstruct(f, dist, x)
+        # Vectorize value.
+        yvec = vectorize(dist, y)
+        # Add it to the new metadata.
+        push!(lengths_new, length(yvec))
+        # Accumulate the log-abs-det jacobian correction.
+        acclogp!!(varinfo, -logjac)
+        # Mark as no longer transformed.
+        settrans!!(varinfo, false, vn)
+        # Return the vectorized transformed value.
+        return yvec
+    end
+
+    # Determine new ranges.
+    ranges_new = similar(metadata.ranges, 0)
+    offset = 0
+    for l in lengths_new
+        r_start, r_end = offset + 1, l + offset
+        offset = r_end
+        push!(ranges_new, r_start:r_end)
+    end
+
+    # Now we just create a new metadata with the new `vals` and `ranges`.
+    return Metadata(
+        metadata.idcs,
+        metadata.vns,
+        ranges_new,
+        vals_new,
+        metadata.dists,
+        metadata.gids,
+        metadata.orders,
+        metadata.flags
+    )
+end
+
 """
     islinked(vi::VarInfo, spl::Union{Sampler, SampleFromPrior})
 
