a18b435048f1639dbd0c848dc07bd6cbd1188aba
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2023-09-06 07:52:47 +0100

    moved generated_quantities tests to test/model.jl

--- test/model.jl
+++ test/model.jl
@@ -278,4 +278,37 @@ end
             @test DynamicPPL.TestUtils.posterior_mean(model) isa typeof(x)
         end
     end
+
+    @testset "generated_quantities on `LKJCholesky`" begin
+        n = 10
+        d = 2
+        model = DynamicPPL.TestUtils.demo_lkjchol(d)
+        xs = [model().x for _ in 1:n]
+
+        # Extract varnames and values.
+        vns_and_vals_xs = map(
+            collect ∘ Base.Fix1(DynamicPPL.varname_and_value_leaves, @varname(x)), xs
+        )
+        vns = map(first, first(vns_and_vals_xs))
+        vals = map(vns_and_vals_xs) do vns_and_vals
+            map(last, vns_and_vals)
+        end
+
+        # Construct the chain.
+        syms = map(Symbol, vns)
+        vns_to_syms = OrderedDict(zip(vns, syms))
+
+        chain = MCMCChains.Chains(
+            permutedims(stack(vals)),
+            syms;
+            info = (varname_to_symbol = vns_to_syms,)
+        )
+        display(chain)
+
+        # Test!
+        results = generated_quantities(model, chain)
+        for (x_true, result) in zip(xs, results)
+            @test x_true.UL == result.x.UL
+        end
+    end
 end
