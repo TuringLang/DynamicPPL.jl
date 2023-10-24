ad5f2b8b367eb2ae509c073d1e8a553212715c61
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2023-10-24 16:31:33 +0100

    fixed old usage of rand

--- test/varinfo.jl
+++ test/varinfo.jl
@@ -541,7 +541,7 @@ DynamicPPL.getspace(::DynamicPPL.Sampler{MySAlg}) = (:s,)
         @testset "$(model.f)" for model in DynamicPPL.TestUtils.DEMO_MODELS
             vns = DynamicPPL.TestUtils.varnames(model)
             varinfos = DynamicPPL.TestUtils.setup_varinfos(
-                model, rand(model), vns; include_threadsafe=true
+                model, DynamicPPL.TestUtils.rand_prior_true(model), vns; include_threadsafe=true
             )
             @testset "$(short_varinfo_name(varinfo))" for varinfo in varinfos
                 @testset "with itself" begin
