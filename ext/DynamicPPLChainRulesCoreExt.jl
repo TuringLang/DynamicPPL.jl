250dd3da391302d9f379e9acfef9d9229f479dde
David Widmann <devmotion@users.noreply.github.com>
2023-11-21 12:13:07 +0100

    Make ZygoteRules and ChainRulesCore weak dependencies

---
+++ ext/DynamicPPLChainRulesCoreExt.jl
@@ -0,0 +1,26 @@
+module DynamicPPLChainRulesCoreExt
+
+if isdefined(Base, :get_extension)
+    using DynamicPPL: DynamicPPL, BangBang, Distributions
+    using ChainRulesCore: ChainRulesCore
+else
+    using ..DynamicPPL: DynamicPPL, BangBang, Distributions
+    using ..ChainRulesCore: ChainRulesCore
+end
+
+# See https://github.com/TuringLang/Turing.jl/issues/1199
+ChainRulesCore.@non_differentiable BangBang.push!!(
+    vi::DynamicPPL.VarInfo,
+    vn::DynamicPPL.VarName,
+    r,
+    dist::Distributions.Distribution,
+    gidset::Set{DynamicPPL.Selector},
+)
+
+ChainRulesCore.@non_differentiable DynamicPPL.updategid!(
+    vi::DynamicPPL.AbstractVarInfo,
+    vn::DynamicPPL.VarName,
+    spl::DynamicPPL.Sampler,
+)
+
+end # module
