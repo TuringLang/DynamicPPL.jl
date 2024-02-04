89d4a1ac77958f15304bcee514c7e9f4d4d939d7
Xianda Sun <sunxdt@gmail.com>
2024-02-04 12:21:02 +0000

    Change testset description

--- test/ad.jl
+++ test/ad.jl
@@ -1,4 +1,4 @@
-@testset "ReverseDiff test for model $(m.f)" for m in DynamicPPL.TestUtils.DEMO_MODELS
+@testset "Testing AD by comparing gradient using ForwardDiff and ReverseDiff for model $(m.f)" for m in DynamicPPL.TestUtils.DEMO_MODELS
     f = DynamicPPL.LogDensityFunction(m)
     rand_param_values = DynamicPPL.TestUtils.rand_prior_true(m)
     vns = DynamicPPL.TestUtils.varnames(m)
