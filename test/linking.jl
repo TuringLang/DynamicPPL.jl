274ec7ea24df74d1ec958d3790a1e212888ae03c
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2024-04-17 19:59:21 +0100

    Update test/linking.jl

--- test/linking.jl
+++ test/linking.jl
@@ -182,7 +182,8 @@ end
         end
         @testset "ns=$ns" for ns in [
             (3,),
-            # (3, 4), (3, 4, 5)
+            (3, 4), 
+            (3, 4, 5)
         ]
             model = demo_highdim_dirichlet(ns...)
             example_values = rand(NamedTuple, model)
