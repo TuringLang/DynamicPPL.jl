a8bb505ba31cd330870397ebfd83395f1176eb95
Xianda Sun <5433119+sunxd3@users.noreply.github.com>
2024-05-13 17:16:53 +0100

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/prob_macro.jl
+++ src/prob_macro.jl
@@ -1,7 +1,11 @@
 macro logprob_str(str)
-    return :(@warn("`@logprob` is deprecated. Use `loglikelihood`, `logprior`, or `logjoint` instead."))
+    return :(@warn(
+        "`@logprob` is deprecated. Use `loglikelihood`, `logprior`, or `logjoint` instead."
+    ))
 end
 
 macro prob_str(str)
-    return :(@warn("`@prob` is deprecated. Use `loglikelihood`, `logprior`, or `logjoint` instead."))
+    return :(@warn(
+        "`@prob` is deprecated. Use `loglikelihood`, `logprior`, or `logjoint` instead."
+    ))
 end
