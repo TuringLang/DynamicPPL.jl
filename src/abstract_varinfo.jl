28b8eae8d173f7ced29fb4b88a9bd02698025f93
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2024-07-01 20:17:46 +0100

    Replaced usage of `typeof` with `Core.Typeof` in `eltype`

--- src/abstract_varinfo.jl
+++ src/abstract_varinfo.jl
@@ -363,9 +363,8 @@ Determine the default `eltype` of the values returned by `vi[spl]`.
     This method is considered legacy, and is likely to be deprecated in the future.
 """
 function Base.eltype(vi::AbstractVarInfo, spl::Union{AbstractSampler,SampleFromPrior})
-    # TODO(torfjelde): Does this cause issues with Enzyme.jl?
     # TODO(torfjelde): Is there _any_ scenario where this isn't just `typeof(getlogp(vi))`?
-    T = Base.promote_op(getindex, typeof(vi), typeof(spl))
+    T = Base.promote_op(getindex, Core.Typeof(vi), Core.Typeof(spl))
     if T === Union{}
         # In this case `getindex(vi, spl)` errors
         # Let us throw a more descriptive error message
