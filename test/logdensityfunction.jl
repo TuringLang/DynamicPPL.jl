66367a7710921896cb5d1dd1eff5fccc1ce6dced
Xianda Sun <sunxdt@gmail.com>
2024-07-01 14:13:28 +0100

    add some test

--- test/logdensityfunction.jl
+++ test/logdensityfunction.jl
@@ -1,5 +1,23 @@
 using Test, DynamicPPL, LogDensityProblems
 
+@testset "`getmodel` and `setmodel`" begin
+    # TODO: does it worth to test all demo models?
+    model = DynamicPPL.TestUtils.DEMO_MODELS[1]
+    ℓ = DynamicPPL.LogDensityFunction(model)
+    @test DynamicPPL.getmodel(ℓ) == model
+    @test DynamicPPL.setmodel(ℓ, model).model == model
+
+    # ReverseDiff related
+    ∇ℓ = LogDensityProblems.ADgradient(:ReverseDiff, ℓ; compile=Val(false))
+    @test DynamicPPL.getmodel(∇ℓ) == model
+    @test getmodel(DynamicPPL.setmodel(∇ℓ, model)) == model
+    
+    ∇ℓ = LogDensityProblems.ADgradient(:ReverseDiff, ℓ; compile=Val(true))
+    new_∇ℓ = DynamicPPL.setmodel(∇ℓ, model)
+    @test DynamicPPL.getmodel(new_∇ℓ) == model
+    @test new_∇ℓ.ℓ.compiledtape != ∇ℓ.ℓ.compiledtape
+end
+
 @testset "LogDensityFunction" begin
     @testset "$(nameof(model))" for model in DynamicPPL.TestUtils.DEMO_MODELS
         example_values = DynamicPPL.TestUtils.rand_prior_true(model)
