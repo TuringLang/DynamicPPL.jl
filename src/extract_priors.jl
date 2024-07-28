beaeeaa256d3618d155e700ec663c0914cf5cf98
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2024-07-28 16:13:26 +0100

    Fixed potential infinite recursion as suggested by @mhauru

--- src/extract_priors.jl
+++ src/extract_priors.jl
@@ -110,7 +110,7 @@ julia> length(extract_priors(rng, model)[@varname(x)])
 9
 ```
 """
-extract_priors(args...) = extract_priors(Random.default_rng(), args...)
+extract_priors(args::Union{Model,AbstractVarInfo}...) = extract_priors(Random.default_rng(), args...)
 function extract_priors(rng::Random.AbstractRNG, model::Model)
     context = PriorExtractorContext(SamplingContext(rng))
     evaluate!!(model, VarInfo(), context)
