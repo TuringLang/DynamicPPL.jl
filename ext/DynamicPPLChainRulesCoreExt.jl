f35eca668ffe40baa09a7b7eac1d9063fbf739e2
Markus Hauru <mhauru@turing.ac.uk>
2024-09-09 17:10:32 +0100

    Add @non_differentiable calls to work around Zygote limitations

--- ext/DynamicPPLChainRulesCoreExt.jl
+++ ext/DynamicPPLChainRulesCoreExt.jl
@@ -24,4 +24,10 @@ ChainRulesCore.@non_differentiable DynamicPPL.updategid!(
 # No need + causes issues for some AD backends, e.g. Zygote.
 ChainRulesCore.@non_differentiable DynamicPPL.infer_nested_eltype(x)
 
+ChainRulesCore.@non_differentiable DynamicPPL.recontiguify_ranges!(ranges)
+# TODO(mhauru) A workaround for https://github.com/FluxML/Zygote.jl/issues/1523, remove when
+# fixed. Note that this is some serious type piracy. Needed because VarNamedVector uses
+# BitVectors.
+ChainRulesCore.@non_differentiable BitVector(a, b)
+
 end # module
