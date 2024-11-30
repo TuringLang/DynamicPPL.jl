c98fe49a3334f9e67925ae0e579a743b3abe5588
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2024-11-30 10:15:32 +0100

    more debugging: try with new bijectors.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,7 @@
+using Pkg
+# Add Bijectors.jl#torfjelde/logabsdetjac-dist-fix
+Pkg.add(url="https://github.com/TuringLang/Bijectors.jl", rev="torfjelde/logabsdetjac-dist-fix")
+
 using Accessors
 using ADTypes
 using DynamicPPL
@@ -76,63 +80,63 @@ include("test_util.jl")
             include("ext/DynamicPPLJETExt.jl")
         end
 
-        @testset "ad" begin
-            include("ext/DynamicPPLForwardDiffExt.jl")
-            include("ad.jl")
-        end
+        # @testset "ad" begin
+        #     include("ext/DynamicPPLForwardDiffExt.jl")
+        #     include("ad.jl")
+        # end
 
-        @testset "prob and logprob macro" begin
-            @test_throws ErrorException prob"..."
-            @test_throws ErrorException logprob"..."
-        end
+        # @testset "prob and logprob macro" begin
+        #     @test_throws ErrorException prob"..."
+        #     @test_throws ErrorException logprob"..."
+        # end
 
-        @testset "doctests" begin
-            DocMeta.setdocmeta!(
-                DynamicPPL,
-                :DocTestSetup,
-                :(using DynamicPPL, Distributions);
-                recursive=true,
-            )
-            doctestfilters = [
-                # Older versions will show "0 element Array" instead of "Type[]".
-                r"(Any\[\]|0-element Array{.+,[0-9]+})",
-                # Older versions will show "Array{...,1}" instead of "Vector{...}".
-                r"(Array{.+,\s?1}|Vector{.+})",
-                # Older versions will show "Array{...,2}" instead of "Matrix{...}".
-                r"(Array{.+,\s?2}|Matrix{.+})",
-                # Errors from macros sometimes result in `LoadError: LoadError:`
-                # rather than `LoadError:`, depending on Julia version.
-                r"ERROR: (LoadError:\s)+",
-                # Older versions do not have `;;]` but instead just `]` at end of the line
-                # => need to treat `;;]` and `]` as the same, i.e. ignore them if at the end of a line
-                r"(;;){0,1}\]$"m,
-            ]
-            doctest(DynamicPPL; manual=false, doctestfilters=doctestfilters)
-        end
+        # @testset "doctests" begin
+        #     DocMeta.setdocmeta!(
+        #         DynamicPPL,
+        #         :DocTestSetup,
+        #         :(using DynamicPPL, Distributions);
+        #         recursive=true,
+        #     )
+        #     doctestfilters = [
+        #         # Older versions will show "0 element Array" instead of "Type[]".
+        #         r"(Any\[\]|0-element Array{.+,[0-9]+})",
+        #         # Older versions will show "Array{...,1}" instead of "Vector{...}".
+        #         r"(Array{.+,\s?1}|Vector{.+})",
+        #         # Older versions will show "Array{...,2}" instead of "Matrix{...}".
+        #         r"(Array{.+,\s?2}|Matrix{.+})",
+        #         # Errors from macros sometimes result in `LoadError: LoadError:`
+        #         # rather than `LoadError:`, depending on Julia version.
+        #         r"ERROR: (LoadError:\s)+",
+        #         # Older versions do not have `;;]` but instead just `]` at end of the line
+        #         # => need to treat `;;]` and `]` as the same, i.e. ignore them if at the end of a line
+        #         r"(;;){0,1}\]$"m,
+        #     ]
+        #     doctest(DynamicPPL; manual=false, doctestfilters=doctestfilters)
+        # end
     end
 
-    if GROUP == "All" || GROUP == "Downstream"
-        @testset "turing" begin
-            try
-                # activate separate test environment
-                Pkg.activate(DIRECTORY_Turing_tests)
-                Pkg.develop(PackageSpec(; path=DIRECTORY_DynamicPPL))
-                Pkg.instantiate()
+    # if GROUP == "All" || GROUP == "Downstream"
+    #     @testset "turing" begin
+    #         try
+    #             # activate separate test environment
+    #             Pkg.activate(DIRECTORY_Turing_tests)
+    #             Pkg.develop(PackageSpec(; path=DIRECTORY_DynamicPPL))
+    #             Pkg.instantiate()
 
-                # make sure that the new environment is considered `using` and `import` statements
-                # (not added automatically on Julia 1.3, see e.g. PR #209)
-                if !(joinpath(DIRECTORY_Turing_tests, "Project.toml") in Base.load_path())
-                    pushfirst!(LOAD_PATH, DIRECTORY_Turing_tests)
-                end
+    #             # make sure that the new environment is considered `using` and `import` statements
+    #             # (not added automatically on Julia 1.3, see e.g. PR #209)
+    #             if !(joinpath(DIRECTORY_Turing_tests, "Project.toml") in Base.load_path())
+    #                 pushfirst!(LOAD_PATH, DIRECTORY_Turing_tests)
+    #             end
 
-                include(joinpath("turing", "runtests.jl"))
-            catch err
-                err isa Pkg.Resolve.ResolverError || rethrow()
-                # If we can't resolve that means this is incompatible by SemVer and this is fine
-                # It means we marked this as a breaking change, so we don't need to worry about
-                # Mistakenly introducing a breaking change, as we have intentionally made one
-                @info "Not compatible with this release. No problem." exception = err
-            end
-        end
-    end
+    #             include(joinpath("turing", "runtests.jl"))
+    #         catch err
+    #             err isa Pkg.Resolve.ResolverError || rethrow()
+    #             # If we can't resolve that means this is incompatible by SemVer and this is fine
+    #             # It means we marked this as a breaking change, so we don't need to worry about
+    #             # Mistakenly introducing a breaking change, as we have intentionally made one
+    #             @info "Not compatible with this release. No problem." exception = err
+    #         end
+    #     end
+    # end
 end
