57f3de33c5cf4a694531605468d2c32637994396
Will Tebbutt <wt0881@my.bristol.ac.uk>
2024-12-08 11:07:06 +0000

    Update test/ext/DynamicPPLMooncakeExt.jl

--- test/ext/DynamicPPLMooncakeExt.jl
+++ test/ext/DynamicPPLMooncakeExt.jl
@@ -1,3 +1,3 @@
 @testset "DynamicPPLMooncakeExt" begin
-    Mooncake.TestUtils.test_rule(StableRNG(123456), istrans, VarInfo(); unsafe_perturb=true)
+    Mooncake.TestUtils.test_rule(StableRNG(123456), istrans, VarInfo(); unsafe_perturb=true, interface_only=true)
 end
