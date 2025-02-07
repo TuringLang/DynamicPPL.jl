35c01e376d611acdca9e995fb6ea11e83c4d6092
Markus Hauru <mhauru@turing.ac.uk>
2025-02-07 17:16:08 +0000

    Remove tests with multivariates or arrays as RHS of .~

--- test/context_implementations.jl
+++ test/context_implementations.jl
@@ -4,7 +4,7 @@
         @model function test(x)
             μ ~ MvNormal(zeros(2), 4 * I)
             z = Vector{Int}(undef, length(x))
-            z .~ Categorical.(fill([0.5, 0.5], length(x)))
+            z ~ product_distribution(Categorical.(fill([0.5, 0.5], length(x))))
             for i in 1:length(x)
                 x[i] ~ Normal(μ[z[i]], 0.1)
             end
@@ -13,59 +13,36 @@
         test([1, 1, -1])(VarInfo(), SampleFromPrior(), LikelihoodContext())
     end
 
-    # https://github.com/TuringLang/DynamicPPL.jl/issues/28#issuecomment-829223577
-    @testset "dot tilde: arrays of distributions" begin
+    @testset "dot tilde with varying sizes" begin
         @testset "assume" begin
             @model function test(x, size)
                 y = Array{Float64,length(size)}(undef, size...)
-                y .~ Normal.(x)
+                y .~ Normal(x)
                 return y, getlogp(__varinfo__)
             end
 
             for ysize in ((2,), (2, 3), (2, 3, 4))
-                for x in (
-                    # scalar
-                    randn(),
-                    # drop trailing dimensions
-                    ntuple(i -> randn(ysize[1:i]), length(ysize))...,
-                    # singleton dimensions
-                    ntuple(
-                        i -> randn(ysize[1:(i - 1)]..., 1, ysize[(i + 1):end]...),
-                        length(ysize),
-                    )...,
-                )
-                    model = test(x, ysize)
-                    y, lp = model()
-                    @test lp ≈ sum(logpdf.(Normal.(x), y))
+                x = randn()
+                model = test(x, ysize)
+                y, lp = model()
+                @test lp ≈ sum(logpdf.(Normal.(x), y))
 
-                    ys = [first(model()) for _ in 1:10_000]
-                    @test norm(mean(ys) .- x, Inf) < 0.1
-                    @test norm(std(ys) .- 1, Inf) < 0.1
-                end
+                ys = [first(model()) for _ in 1:10_000]
+                @test norm(mean(ys) .- x, Inf) < 0.1
+                @test norm(std(ys) .- 1, Inf) < 0.1
             end
         end
 
         @testset "observe" begin
             @model function test(x, y)
-                return y .~ Normal.(x)
+                return y .~ Normal(x)
             end
 
             for ysize in ((2,), (2, 3), (2, 3, 4))
-                for x in (
-                    # scalar
-                    randn(),
-                    # drop trailing dimensions
-                    ntuple(i -> randn(ysize[1:i]), length(ysize))...,
-                    # singleton dimensions
-                    ntuple(
-                        i -> randn(ysize[1:(i - 1)]..., 1, ysize[(i + 1):end]...),
-                        length(ysize),
-                    )...,
-                )
-                    y = randn(ysize)
-                    z = logjoint(test(x, y), VarInfo())
-                    @test z ≈ sum(logpdf.(Normal.(x), y))
-                end
+                x = randn()
+                y = randn(ysize)
+                z = logjoint(test(x, y), VarInfo())
+                @test z ≈ sum(logpdf.(Normal.(x), y))
             end
         end
     end
