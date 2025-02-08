a34ae455b23ea2beb8c4fea5d2eecf0d50a0df29
Penelope Yong <penelopeysm@gmail.com>
2025-02-08 18:14:16 +0000

    (DROP) Comment out all other tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -45,57 +45,54 @@ include("test_util.jl")
     # groups are chosen to make both groups take roughly the same amount of
     # time, but beyond that there is no particular reason for the split.
     if GROUP == "All" || GROUP == "Group1"
-        include("utils.jl")
-        include("compiler.jl")
-        include("varnamedvector.jl")
-        include("varinfo.jl")
-        include("simple_varinfo.jl")
-        include("model.jl")
-        include("sampler.jl")
-        include("independence.jl")
-        include("distribution_wrappers.jl")
-        include("logdensityfunction.jl")
-        include("linking.jl")
-        include("serialization.jl")
-        include("pointwise_logdensities.jl")
-        include("lkj.jl")
-        include("deprecated.jl")
+        # include("utils.jl")
+        # include("compiler.jl")
+        # include("varnamedvector.jl")
+        # include("varinfo.jl")
+        # include("simple_varinfo.jl")
+        # include("model.jl")
+        # include("sampler.jl")
+        # include("independence.jl")
+        # include("distribution_wrappers.jl")
+        # include("logdensityfunction.jl")
+        # include("linking.jl")
+        # include("serialization.jl")
+        # include("pointwise_logdensities.jl")
+        # include("lkj.jl")
+        # include("deprecated.jl")
     end
 
     if GROUP == "All" || GROUP == "Group2"
-        include("contexts.jl")
-        include("context_implementations.jl")
-        include("threadsafe.jl")
-        include("debug_utils.jl")
-        @testset "compat" begin
-            include(joinpath("compat", "ad.jl"))
-        end
-        @testset "extensions" begin
-            include("ext/DynamicPPLMCMCChainsExt.jl")
-            include("ext/DynamicPPLJETExt.jl")
-        end
+        # include("contexts.jl")
+        # include("context_implementations.jl")
+        # include("threadsafe.jl")
+        # include("debug_utils.jl")
+        # @testset "extensions" begin
+        #     include("ext/DynamicPPLMCMCChainsExt.jl")
+        #     include("ext/DynamicPPLJETExt.jl")
+        # end
         @testset "ad" begin
-            include("ext/DynamicPPLForwardDiffExt.jl")
-            include("ext/DynamicPPLMooncakeExt.jl")
+            # include("ext/DynamicPPLForwardDiffExt.jl")
+            # include("ext/DynamicPPLMooncakeExt.jl")
             include("ad.jl")
         end
-        @testset "prob and logprob macro" begin
-            @test_throws ErrorException prob"..."
-            @test_throws ErrorException logprob"..."
-        end
-        @testset "doctests" begin
-            DocMeta.setdocmeta!(
-                DynamicPPL,
-                :DocTestSetup,
-                :(using DynamicPPL, Distributions);
-                recursive=true,
-            )
-            doctestfilters = [
-                # Ignore the source of a warning in the doctest output, since this is dependent on host.
-                # This is a line that starts with "└ @ " and ends with the line number.
-                r"└ @ .+:[0-9]+",
-            ]
-            doctest(DynamicPPL; manual=false, doctestfilters=doctestfilters)
-        end
+        # @testset "prob and logprob macro" begin
+        #     @test_throws ErrorException prob"..."
+        #     @test_throws ErrorException logprob"..."
+        # end
+        # @testset "doctests" begin
+        #     DocMeta.setdocmeta!(
+        #         DynamicPPL,
+        #         :DocTestSetup,
+        #         :(using DynamicPPL, Distributions);
+        #         recursive=true,
+        #     )
+        #     doctestfilters = [
+        #         # Ignore the source of a warning in the doctest output, since this is dependent on host.
+        #         # This is a line that starts with "└ @ " and ends with the line number.
+        #         r"└ @ .+:[0-9]+",
+        #     ]
+        #     doctest(DynamicPPL; manual=false, doctestfilters=doctestfilters)
+        # end
     end
 end
