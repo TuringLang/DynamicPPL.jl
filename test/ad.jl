d1a98c632474ecd139ae36082aeeef602a3fa2a7
Markus Hauru <markus@mhauru.org>
2025-03-17 18:02:25 +0000

    Add a couple of tests being removed from Turing.jl (#840)
    * Add a couple of tests being removed from Turing.jl
    
    * Simplify a test
    
    Co-authored-by: Penelope Yong <penelopeysm@gmail.com>
    
    * Simplify ad tests
    
    ---------
    
    Co-authored-by: Penelope Yong <penelopeysm@gmail.com>

--- test/ad.jl
+++ test/ad.jl
@@ -1,6 +1,14 @@
 using DynamicPPL: LogDensityFunction
 
 @testset "Automatic differentiation" begin
+    # Used as the ground truth that others are compared against.
+    ref_adtype = AutoForwardDiff()
+    test_adtypes = [
+        AutoReverseDiff(; compile=false),
+        AutoReverseDiff(; compile=true),
+        AutoMooncake(; config=nothing),
+    ]
+
     @testset "Unsupported backends" begin
         @model demo() = x ~ Normal()
         @test_logs (:warn, r"not officially supported") LogDensityFunction(
@@ -18,15 +26,10 @@ using DynamicPPL: LogDensityFunction
                 f = LogDensityFunction(m, varinfo)
                 x = DynamicPPL.getparams(f)
                 # Calculate reference logp + gradient of logp using ForwardDiff
-                ref_adtype = ADTypes.AutoForwardDiff()
                 ref_ldf = LogDensityFunction(m, varinfo; adtype=ref_adtype)
                 ref_logp, ref_grad = LogDensityProblems.logdensity_and_gradient(ref_ldf, x)
 
-                @testset "$adtype" for adtype in [
-                    AutoReverseDiff(; compile=false),
-                    AutoReverseDiff(; compile=true),
-                    AutoMooncake(; config=nothing),
-                ]
+                @testset "$adtype" for adtype in test_adtypes
                     @info "Testing AD on: $(m.f) - $(short_varinfo_name(varinfo)) - $adtype"
 
                     # Put predicates here to avoid long lines
@@ -102,5 +105,67 @@ using DynamicPPL: LogDensityFunction
             model, vi, SamplingContext(spl); adtype=AutoReverseDiff(; compile=true)
         )
         @test LogDensityProblems.logdensity_and_gradient(ldf, vi[:]) isa Any
+    end
+
+    # Test that various different ways of specifying array types as arguments work with all
+    # ADTypes.
+    @testset "Array argument types" begin
+        test_m = randn(2, 3)
+
+        function eval_logp_and_grad(model, m, adtype)
+            ldf = LogDensityFunction(model(); adtype=adtype)
+            return LogDensityProblems.logdensity_and_gradient(ldf, m[:])
+        end
+
+        @model function scalar_matrix_model(::Type{T}=Float64) where {T<:Real}
+            m = Matrix{T}(undef, 2, 3)
+            return m ~ filldist(MvNormal(zeros(2), I), 3)
+        end
+
+        scalar_matrix_model_reference = eval_logp_and_grad(
+            scalar_matrix_model, test_m, ref_adtype
+        )
+
+        @model function matrix_model(::Type{T}=Matrix{Float64}) where {T}
+            m = T(undef, 2, 3)
+            return m ~ filldist(MvNormal(zeros(2), I), 3)
+        end
+
+        matrix_model_reference = eval_logp_and_grad(matrix_model, test_m, ref_adtype)
+
+        @model function scalar_array_model(::Type{T}=Float64) where {T<:Real}
+            m = Array{T}(undef, 2, 3)
+            return m ~ filldist(MvNormal(zeros(2), I), 3)
+        end
+
+        scalar_array_model_reference = eval_logp_and_grad(
+            scalar_array_model, test_m, ref_adtype
+        )
+
+        @model function array_model(::Type{T}=Array{Float64}) where {T}
+            m = T(undef, 2, 3)
+            return m ~ filldist(MvNormal(zeros(2), I), 3)
+        end
+
+        array_model_reference = eval_logp_and_grad(array_model, test_m, ref_adtype)
+
+        @testset "$adtype" for adtype in test_adtypes
+            scalar_matrix_model_logp_and_grad = eval_logp_and_grad(
+                scalar_matrix_model, test_m, adtype
+            )
+            @test scalar_matrix_model_logp_and_grad[1] ≈ scalar_matrix_model_reference[1]
+            @test scalar_matrix_model_logp_and_grad[2] ≈ scalar_matrix_model_reference[2]
+            matrix_model_logp_and_grad = eval_logp_and_grad(matrix_model, test_m, adtype)
+            @test matrix_model_logp_and_grad[1] ≈ matrix_model_reference[1]
+            @test matrix_model_logp_and_grad[2] ≈ matrix_model_reference[2]
+            scalar_array_model_logp_and_grad = eval_logp_and_grad(
+                scalar_array_model, test_m, adtype
+            )
+            @test scalar_array_model_logp_and_grad[1] ≈ scalar_array_model_reference[1]
+            @test scalar_array_model_logp_and_grad[2] ≈ scalar_array_model_reference[2]
+            array_model_logp_and_grad = eval_logp_and_grad(array_model, test_m, adtype)
+            @test array_model_logp_and_grad[1] ≈ array_model_reference[1]
+            @test array_model_logp_and_grad[2] ≈ array_model_reference[2]
+        end
     end
 end
