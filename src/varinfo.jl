ebe409396c88d54bb0c6359a0109acdac7fa4f68
Penelope Yong <penelopeysm@gmail.com>
2025-03-21 14:49:05 +0000

    Re-add but deprecate VarInfo(::VarInfo, ::AbstractVector)

--- src/varinfo.jl
+++ src/varinfo.jl
@@ -100,6 +100,8 @@ const TypedVarInfo = VarInfo{<:NamedTuple}
 const VarInfoOrThreadSafeVarInfo{Tmeta} = Union{
     VarInfo{Tmeta},ThreadSafeVarInfo{<:VarInfo{Tmeta}}
 }
+# TODO: Remove this
+@deprecate VarInfo(vi::VarInfo, x::AbstractVector) unflatten(vi, x)
 
 # NOTE: This is kind of weird, but it effectively preserves the "old"
 # behavior where we're allowed to call `link!` on the same `VarInfo`
