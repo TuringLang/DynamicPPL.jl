d5101574eb3e9d3060a13084e341aa43978f3800
Penelope Yong <penelopeysm@gmail.com>
2025-04-04 01:50:32 +0100

    Implement AD testing and benchmarking (hand rolled)

--- src/DynamicPPL.jl
+++ src/DynamicPPL.jl
@@ -175,7 +175,6 @@ include("context_implementations.jl")
 include("compiler.jl")
 include("pointwise_logdensities.jl")
 include("submodel_macro.jl")
-include("test_utils.jl")
 include("transforming.jl")
 include("logdensityfunction.jl")
 include("model_utils.jl")
@@ -184,6 +183,7 @@ include("values_as_in_model.jl")
 
 include("debug_utils.jl")
 using .DebugUtils
+include("test_utils.jl")
 
 include("experimental.jl")
 include("deprecated.jl")
