70a63471704b2407108eea256df7e0278b8b1aa8
Penelope Yong <penelopeysm@gmail.com>
2025-06-02 10:36:53 +0100

    Use Threads.nthreads() * 2 in TSVI

--- src/simple_varinfo.jl
+++ src/simple_varinfo.jl
@@ -648,10 +648,12 @@ end
 # Threadsafe stuff.
 # For `SimpleVarInfo` we don't really need `Ref` so let's not use it.
 function ThreadSafeVarInfo(vi::SimpleVarInfo)
-    return ThreadSafeVarInfo(vi, zeros(typeof(getlogp(vi)), Threads.nthreads()))
+    return ThreadSafeVarInfo(vi, zeros(typeof(getlogp(vi)), Threads.nthreads() * 2))
 end
 function ThreadSafeVarInfo(vi::SimpleVarInfo{<:Any,<:Ref})
-    return ThreadSafeVarInfo(vi, [Ref(zero(getlogp(vi))) for _ in 1:Threads.nthreads()])
+    return ThreadSafeVarInfo(
+        vi, [Ref(zero(getlogp(vi))) for _ in 1:(Threads.nthreads() * 2)]
+    )
 end
 
 has_varnamedvector(vi::SimpleVarInfo) = vi.values isa VarNamedVector
