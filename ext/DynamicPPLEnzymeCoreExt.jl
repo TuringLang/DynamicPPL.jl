9123b2b20685ec849938e31269e181578b1cc71e
Penelope Yong <penelopeysm@gmail.com>
2025-11-06 02:43:55 +0000

    Mark `get_range_and_linked` as having zero derivative

--- ext/DynamicPPLEnzymeCoreExt.jl
+++ ext/DynamicPPLEnzymeCoreExt.jl
@@ -1,16 +1,15 @@
 module DynamicPPLEnzymeCoreExt
 
-if isdefined(Base, :get_extension)
-    using DynamicPPL: DynamicPPL
-    using EnzymeCore
-else
-    using ..DynamicPPL: DynamicPPL
-    using ..EnzymeCore
-end
+using DynamicPPL: DynamicPPL
+using EnzymeCore
 
 # Mark is_transformed as having 0 derivative. The `nothing` return value is not significant, Enzyme
 # only checks whether such a method exists, and never runs it.
 @inline EnzymeCore.EnzymeRules.inactive(::typeof(DynamicPPL.is_transformed), args...) =
     nothing
+# Likewise for get_range_and_linked.
+@inline EnzymeCore.EnzymeRules.inactive(
+    ::typeof(DynamicPPL.get_range_and_linked), args...
+) = nothing
 
 end
