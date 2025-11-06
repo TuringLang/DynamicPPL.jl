4cefaca4803107da0570430277a3f05a27ae2146
Penelope Yong <penelopeysm@gmail.com>
2025-11-06 02:43:55 +0000

    Mark `get_range_and_linked` as having zero derivative

--- ext/DynamicPPLMooncakeExt.jl
+++ ext/DynamicPPLMooncakeExt.jl
@@ -1,9 +1,10 @@
 module DynamicPPLMooncakeExt
 
-using DynamicPPL: DynamicPPL, is_transformed
+using DynamicPPL: DynamicPPL, is_transformed, get_range_and_linked
 using Mooncake: Mooncake
 
 # This is purely an optimisation.
 Mooncake.@zero_derivative Mooncake.DefaultCtx Tuple{typeof(is_transformed),Vararg}
+Mooncake.@zero_derivative Mooncake.DefaultCtx Tuple{typeof(get_range_and_linked),Vararg}
 
 end # module
