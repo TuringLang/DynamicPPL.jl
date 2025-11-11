62a8746c20431cefe2514e4716d44d0364a01328
Penelope Yong <penelopeysm@gmail.com>
2025-11-11 21:37:43 +0000

    Update comment

--- src/onlyaccs.jl
+++ src/onlyaccs.jl
@@ -2,14 +2,16 @@
     OnlyAccsVarInfo
 
 This is a wrapper around an `AccumulatorTuple` that implements the minimal `AbstractVarInfo`
-interface to work with the `accumulate_assume!!` and `accumulate_observe!!` functions.
+interface to work with the `tilde_assume!!` and `tilde_observe!!` functions for
+`InitContext`.
 
 Note that this does not implement almost every other AbstractVarInfo interface function, and
-so using this outside of FastLDF will lead to errors.
+so using attempting to use this with a different leaf context such as `DefaultContext` will
+result in errors.
 
 Conceptually, one can also think of this as a VarInfo that doesn't contain a metadata field.
-That is because values for random variables are obtained by reading from a separate entity
-(such as a `FastLDFContext`), rather than from the VarInfo itself.
+This is also why it only works with `InitContext`: in this case, the parameters used for
+evaluation are supplied by the context instead of the metadata.
 """
 struct OnlyAccsVarInfo{Accs<:AccumulatorTuple} <: AbstractVarInfo
     accs::Accs
@@ -19,7 +21,7 @@ function OnlyAccsVarInfo(accs::NTuple{N,AbstractAccumulator}) where {N}
     return OnlyAccsVarInfo(AccumulatorTuple(accs))
 end
 
-# AbstractVarInfo interface
+# Minimal AbstractVarInfo interface
 @inline DynamicPPL.maybe_invlink_before_eval!!(vi::OnlyAccsVarInfo, ::Model) = vi
 @inline DynamicPPL.getaccs(vi::OnlyAccsVarInfo) = vi.accs
 @inline DynamicPPL.setaccs!!(::OnlyAccsVarInfo, accs::AccumulatorTuple) =
