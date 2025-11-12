19af4cc3f4617ec0dbac52e6052945faa3456bd3
Penelope Yong <penelopeysm@gmail.com>
2025-11-12 15:32:35 +0000

    Remove inlines, fix OAVI docstring

--- src/onlyaccs.jl
+++ src/onlyaccs.jl
@@ -6,8 +6,7 @@ interface to work with the `tilde_assume!!` and `tilde_observe!!` functions for
 `InitContext`.
 
 Note that this does not implement almost every other AbstractVarInfo interface function, and
-so using attempting to use this with a different leaf context such as `DefaultContext` will
-result in errors.
+so using this with a different leaf context such as `DefaultContext` will result in errors.
 
 Conceptually, one can also think of this as a VarInfo that doesn't contain a metadata field.
 This is also why it only works with `InitContext`: in this case, the parameters used for
@@ -22,10 +21,9 @@ function OnlyAccsVarInfo(accs::NTuple{N,AbstractAccumulator}) where {N}
 end
 
 # Minimal AbstractVarInfo interface
-@inline DynamicPPL.maybe_invlink_before_eval!!(vi::OnlyAccsVarInfo, ::Model) = vi
-@inline DynamicPPL.getaccs(vi::OnlyAccsVarInfo) = vi.accs
-@inline DynamicPPL.setaccs!!(::OnlyAccsVarInfo, accs::AccumulatorTuple) =
-    OnlyAccsVarInfo(accs)
-@inline Base.haskey(::OnlyAccsVarInfo, ::VarName) = false
-@inline DynamicPPL.is_transformed(::OnlyAccsVarInfo) = false
-@inline BangBang.push!!(vi::OnlyAccsVarInfo, vn, y, dist) = vi
+DynamicPPL.maybe_invlink_before_eval!!(vi::OnlyAccsVarInfo, ::Model) = vi
+DynamicPPL.getaccs(vi::OnlyAccsVarInfo) = vi.accs
+DynamicPPL.setaccs!!(::OnlyAccsVarInfo, accs::AccumulatorTuple) = OnlyAccsVarInfo(accs)
+Base.haskey(::OnlyAccsVarInfo, ::VarName) = false
+DynamicPPL.is_transformed(::OnlyAccsVarInfo) = false
+BangBang.push!!(vi::OnlyAccsVarInfo, vn, y, dist) = vi
