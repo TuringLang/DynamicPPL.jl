7af9c2ec546fe5fb1f21c3251d923fdc93dcabbd
Penelope Yong <penelopeysm@gmail.com>
2025-11-12 15:53:09 +0000

    Replace map(identity, x[:]) with [i for i in x[:]]

--- test/fasteval.jl
+++ test/fasteval.jl
@@ -37,7 +37,7 @@ end
                     unlinked_vi
                 end
                 nt_ranges, dict_ranges = DynamicPPL.Experimental.get_ranges_and_linked(vi)
-                params = map(identity, vi[:])
+                params = [x for x in vi[:]]
                 # Iterate over all variables
                 for vn in keys(vi)
                     # Check that `getindex_internal` returns the same thing as using the ranges
@@ -147,7 +147,7 @@ end
             varinfo = VarInfo(m)
             linked_varinfo = DynamicPPL.link(varinfo, m)
             f = FastLDF(m, getlogjoint_internal, linked_varinfo)
-            x = map(identity, linked_varinfo[:])
+            x = [p for p in linked_varinfo[:]]
 
             # Calculate reference logp + gradient of logp using ForwardDiff
             ref_ad_result = run_ad(m, ref_adtype; varinfo=linked_varinfo, test=NoTest())
