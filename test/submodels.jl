47977009a747dc581994b80aef595984f9064b11
Penelope Yong <penelopeysm@gmail.com>
2026-03-17 23:53:07 +0000

    Fix submodel tests

--- test/submodels.jl
+++ test/submodels.jl
@@ -10,6 +10,13 @@ mutable struct P
     b::Float64
 end
 
+function get_logp_and_rawval_accs(model::Model)
+    accs = OnlyAccsVarInfo()
+    accs = setacc!!(accs, RawValueAccumulator(false))
+    _, accs = init!!(model, accs, InitFromPrior(), UnlinkAll())
+    return accs
+end
+
 @testset "submodels.jl" begin
     @testset "$op with AbstractPPL API" for op in [condition, fix]
         x_val = 1.0
@@ -40,10 +47,12 @@ end
                 # Test that the value was correctly set
                 @test model()[1] == x_val
                 # Test that the logp was correctly set
-                vi = VarInfo(model)
-                @test getlogjoint(vi) == x_logp + logpdf(Normal(), vi[@varname(a.y)])
+                accs = get_logp_and_rawval_accs(model)
+                raw_vals = get_raw_values(accs)
+                @test getlogjoint(accs) ==
+                    x_logp + logpdf(Normal(), raw_vals[@varname(a.y)])
                 # Check the keys
-                @test Set(keys(VarInfo(model))) == Set([@varname(a.y)])
+                @test Set(keys(raw_vals)) == Set([@varname(a.y)])
             end
         end
 
@@ -72,10 +81,11 @@ end
                 # Test that the value was correctly set
                 @test model()[1] == x_val
                 # Test that the logp was correctly set
-                vi = VarInfo(model)
-                @test getlogjoint(vi) == x_logp + logpdf(Normal(), vi[@varname(y)])
+                accs = get_logp_and_rawval_accs(model)
+                raw_vals = get_raw_values(accs)
+                @test getlogjoint(accs) == x_logp + logpdf(Normal(), raw_vals[@varname(y)])
                 # Check the keys
-                @test Set(keys(VarInfo(model))) == Set([@varname(y)])
+                @test Set(keys(raw_vals)) == Set([@varname(y)])
             end
         end
 
@@ -104,10 +114,12 @@ end
                 # Test that the value was correctly set
                 @test model()[1] == x_val
                 # Test that the logp was correctly set
-                vi = VarInfo(model)
-                @test getlogjoint(vi) == x_logp + logpdf(Normal(), vi[@varname(b.y)])
+                accs = get_logp_and_rawval_accs(model)
+                raw_vals = get_raw_values(accs)
+                @test getlogjoint(accs) ==
+                    x_logp + logpdf(Normal(), raw_vals[@varname(b.y)])
                 # Check the keys
-                @test Set(keys(VarInfo(model))) == Set([@varname(b.y)])
+                @test Set(keys(raw_vals)) == Set([@varname(b.y)])
             end
         end
 
@@ -125,13 +137,13 @@ end
                 return (p.a, p.b)
             end
             expected_vns = Set([@varname(p.a.x[1]), @varname(p.a.y), @varname(p.b)])
-            @test Set(keys(VarInfo(g()))) == expected_vns
+            @test Set(keys(rand(g()))) == expected_vns
 
             # Check that we can condition/fix on any of them from the outside
             for vn in expected_vns
                 op_g = op(g(), (vn => 1.0))
-                vi = VarInfo(op_g)
-                @test Set(keys(vi)) == symdiff(expected_vns, Set([vn]))
+                vnt = rand(op_g)
+                @test Set(keys(vnt)) == symdiff(expected_vns, Set([vn]))
             end
         end
 
@@ -148,11 +160,12 @@ end
             end
 
             # No conditioning
-            vi = VarInfo(h())
-            @test Set(keys(vi)) == Set([@varname(a.b.x), @varname(a.b.y)])
-            @test getlogjoint(vi) ==
-                logpdf(Normal(), vi[@varname(a.b.x)]) +
-                  logpdf(Normal(), vi[@varname(a.b.y)])
+            accs = get_logp_and_rawval_accs(h())
+            raw_vals = get_raw_values(accs)
+            @test Set(keys(raw_vals)) == Set([@varname(a.b.x), @varname(a.b.y)])
+            @test getlogjoint(accs) ==
+                logpdf(Normal(), raw_vals[@varname(a.b.x)]) +
+                  logpdf(Normal(), raw_vals[@varname(a.b.y)])
 
             # Conditioning/fixing at the top level
             op_h = op(h(), (@varname(a.b.x) => x_val))
@@ -174,9 +187,11 @@ end
 
             models = [("top", op_h), ("middle", h2()), ("bottom", h3())]
             @testset "$name" for (name, model) in models
-                vi = VarInfo(model)
-                @test Set(keys(vi)) == Set([@varname(a.b.y)])
-                @test getlogjoint(vi) == x_logp + logpdf(Normal(), vi[@varname(a.b.y)])
+                accs = get_logp_and_rawval_accs(model)
+                raw_vals = get_raw_values(accs)
+                @test Set(keys(raw_vals)) == Set([@varname(a.b.y)])
+                @test getlogjoint(accs) ==
+                    x_logp + logpdf(Normal(), raw_vals[@varname(a.b.y)])
             end
         end
     end
@@ -190,11 +205,11 @@ end
             return a ~ to_submodel(f(inner_x))
         end
 
-        vi = VarInfo(g(1.0))
-        @test Set(keys(vi)) == Set([@varname(a.y)])
+        vnt = rand(g(1.0))
+        @test Set(keys(vnt)) == Set([@varname(a.y)])
 
-        vi = VarInfo(g(missing))
-        @test Set(keys(vi)) == Set([@varname(a.x), @varname(a.y)])
+        vnt = rand(g(missing))
+        @test Set(keys(vnt)) == Set([@varname(a.x), @varname(a.y)])
     end
 
     @testset ":= in submodels" begin
@@ -210,12 +225,12 @@ end
             end
 
             model = outer1()
-            a, vi = DynamicPPL.init!!(model, VarInfo())
-            @test only(keys(vi)) == @varname(x.a)
+            vnt = rand(model)
+            @test only(keys(vnt)) == @varname(x.a)
 
-            vi = OnlyAccsVarInfo((RawValueAccumulator(true),))
-            _, vi = init!!(model, vi, InitFromPrior(), UnlinkAll())
-            vnt = get_raw_values(vi)
+            accs = OnlyAccsVarInfo((RawValueAccumulator(true),))
+            a, accs = init!!(model, accs, InitFromPrior(), UnlinkAll())
+            vnt = get_raw_values(accs)
             @test vnt[@varname(x.a)] == a
             @test vnt[@varname(x.b)] == vnt[@varname(x.a)] + 1.0
         end
@@ -235,12 +250,12 @@ end
             end
 
             model = outer2()
-            a, vi = DynamicPPL.init!!(model, VarInfo())
-            @test only(keys(vi)) == @varname(x.a)
+            vnt = rand(model)
+            @test only(keys(vnt)) == @varname(x.a)
 
-            vi = OnlyAccsVarInfo((RawValueAccumulator(true),))
-            _, vi = init!!(model, vi, InitFromPrior(), UnlinkAll())
-            vnt = get_raw_values(vi)
+            accs = OnlyAccsVarInfo((RawValueAccumulator(true),))
+            a, accs = init!!(model, accs, InitFromPrior(), UnlinkAll())
+            vnt = get_raw_values(accs)
             @test vnt[@varname(x.a)] == a
             @test vnt[@varname(x.b[1])] == vnt[@varname(x.a)] + 1.0
             # If the templating fails, then x.b will be stored as a GrowableArray, and
@@ -288,14 +303,11 @@ end
             end
         end
         model = outer()
-        vi = VarInfo(model)
-        @test Set(keys(vi)) == Set([@varname(x[i].a) for i in 1:4])
+        vnt = rand(model)
+        @test Set(keys(vnt)) == Set([@varname(x[i].a) for i in 1:4])
         for i in 1:4
-            # Need to be careful about what we're testing here. If we do vi[vn], then it
-            # expects that vi.values[vn] isa TransformedValue. That is true of the inner
-            # keys (x[i].a), but x[i] is not itself a key.
-            @test vi.values[@varname(x[i])] isa VarNamedTuple
-            @test vi[@varname(x[i].a)] isa Float64
+            @test vnt[@varname(x[i])] isa VarNamedTuple
+            @test vnt[@varname(x[i].a)] isa Float64
         end
     end
 end
