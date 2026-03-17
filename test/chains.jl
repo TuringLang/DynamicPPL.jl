696e754908d58821df40a258a2549147c29e2c14
Penelope Yong <penelopeysm@gmail.com>
2026-03-17 23:43:16 +0000

    fix chains test

--- test/chains.jl
+++ test/chains.jl
@@ -69,46 +69,25 @@ using Test
     end
 end
 
-_safe_length(x) = length(x)
-# This actually gives N^2 elements, although there are only really N(N+1)/2 parameters in
-# the Cholesky factor. It doesn't really matter because we are comparing like for like i.e.
-# both sides of the sum will have the same overcounting.
-_safe_length(c::LinearAlgebra.Cholesky) = length(c.UL)
-
 @testset "ParamsWithStats from LogDensityFunction" begin
     @testset "$(m.f)" for m in DynamicPPL.TestUtils.ALL_MODELS
         @testset "$transform_strategy" for transform_strategy in (UnlinkAll(), LinkAll())
-            vi = VarInfo(m, InitFromPrior(), transform_strategy)
-            params = [x for x in vi[:]]
-
             # Get the ParamsWithStats using LogDensityFunction
-            ldf = DynamicPPL.LogDensityFunction(m, getlogjoint, vi)
-            ps = ParamsWithStats(params, ldf)
-
-            # The keys are not necessarily going to be the same, because `ps.params` was
-            # obtained via RawValueAccumulator, which only stores raw values. However, `vi`
-            # stores TransformedValue objects. So, if you have something like
-            #    x[4:5] ~ MvNormal(zeros(2), I)
-            # then `ps.params` will have keys `x[4]` and `x[5]` (since it just contains a
-            # PartialArray with those two elements unmasked), whereas `vi` will have the key
-            # `x[4:5]` which stores an ArrayLikeBlock with two elements.
-            # 
-            # On top of that, the ParamsWithStats VNT will also have been `densify!!`-ed, so
-            # it may well just store a single vector `x` rather than individual `x[i]`'s.
-            #
-            # What we CAN do, though, is to check the size of the thing obtained by
-            # indexing into the keys. For `ps.params`, indexing into `x[4]` and `x[5]` will
-            # give two floats, each of "length" 1. For `vi`, indexing into `x[4:5]` will
-            # give a single object that has length 2. So we can check that the total number
-            # of _things_ contained inside is the same.
-            #
-            # Unfortunately, we need _safe_length to handle Cholesky.
-            @test sum(_safe_length(ps.params[vn]) for vn in keys(ps.params)) ==
-                sum(_safe_length(vi[vn]) for vn in keys(vi))
+            ldf = DynamicPPL.LogDensityFunction(m, getlogjoint, transform_strategy)
+            param_vector = rand(ldf)
+            # This will give us a VNT of values.params`.
+            actual_vnt = ParamsWithStats(param_vector, ldf).params
+            # We should make sure that those values line up with the values inside the vector.
+            accs = OnlyAccsVarInfo(RawValueAccumulator(true))
+            _, accs = DynamicPPL.init!!(
+                m, accs, InitFromVector(param_vector, ldf), transform_strategy
+            )
+            expected_vnt = DynamicPPL.densify!!(get_raw_values(accs))
 
             # Iterate over all variables to check that their values match
-            for vn in keys(vi)
-                @test ps.params[vn] == vi[vn]
+            @test Set(keys(actual_vnt)) == Set(keys(expected_vnt))
+            for vn in keys(actual_vnt)
+                @test actual_vnt[vn] == expected_vnt[vn]
             end
         end
     end
