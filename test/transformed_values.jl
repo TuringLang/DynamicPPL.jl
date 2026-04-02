16e4ee2d1de1c4d22f253113a1ff15c0f9c768dc
Penelope Yong <penelopeysm@gmail.com>
2026-04-02 15:19:55 +0100

    Add more tests

--- test/transformed_values.jl
+++ test/transformed_values.jl
@@ -8,6 +8,7 @@ using AbstractPPL: AbstractPPL
 using Bijectors: Bijectors
 using DynamicPPL
 using Distributions
+using Random: Xoshiro
 using Test
 
 @testset "get_fixed_transforms" begin
@@ -43,6 +44,69 @@ using Test
         end
         @test transforms == expected
     end
+end
+
+@testset "infer_transform_strategy_from_values" begin
+    # If all are DynamicLink -> LinkAll
+    vnt_linked = @vnt begin
+        x := TransformedValue([1.0], DynamicLink())
+        y := TransformedValue([2.0, 3.0], DynamicLink())
+    end
+    @test DynamicPPL.infer_transform_strategy_from_values(vnt_linked) isa LinkAll
+
+    # If all are Unlink -> UnlinkAll
+    vnt_unlinked = @vnt begin
+        x := TransformedValue([1.0], Unlink())
+        y := TransformedValue([2.0, 3.0], Unlink())
+    end
+    @test DynamicPPL.infer_transform_strategy_from_values(vnt_unlinked) isa UnlinkAll
+
+    # Mixed -> WithTransforms
+    vnt_mixed = @vnt begin
+        x := TransformedValue([1.0], DynamicLink())
+        y := TransformedValue([2.0, 3.0], Unlink())
+    end
+    strategy = DynamicPPL.infer_transform_strategy_from_values(vnt_mixed)
+    @test strategy isa DynamicPPL.WithTransforms
+    @test DynamicPPL.target_transform(strategy, @varname(x)) isa DynamicLink
+    @test DynamicPPL.target_transform(strategy, @varname(y)) isa Unlink
+
+    # FixedTransform values -> WithTransforms
+    ft = FixedTransform(Bijectors.VectorBijectors.from_linked_vec(Beta(2, 5)))
+    vnt_fixed = @vnt begin
+        x := TransformedValue([0.5], ft)
+    end
+    strategy = DynamicPPL.infer_transform_strategy_from_values(vnt_fixed)
+    @test strategy isa DynamicPPL.WithTransforms
+    @test DynamicPPL.target_transform(strategy, @varname(x)) == ft
+end
+
+@testset "apply_transform_strategy: FixedTransform mismatch errors" begin
+    dist = Beta(2, 5)
+    vn = @varname(x)
+    ft = FixedTransform(Bijectors.VectorBijectors.from_linked_vec(dist))
+    wrong_ft = FixedTransform(Bijectors.VectorBijectors.from_linked_vec(InverseGamma(2, 3)))
+
+    # Create a TransformedValue with ft
+    flink = Bijectors.VectorBijectors.to_linked_vec(dist)
+    raw_val = rand(Xoshiro(468), dist)
+    linked_val, logjac = Bijectors.with_logabsdet_jacobian(flink, raw_val)
+    tv = TransformedValue(linked_val, ft)
+
+    # Matching transform should work
+    strategy_ok = DynamicPPL.WithTransforms(VarNamedTuple(; x=ft), UnlinkAll())
+    new_raw, new_tv, new_logjac = DynamicPPL.apply_transform_strategy(
+        strategy_ok, tv, vn, dist
+    )
+    @test new_raw ≈ raw_val
+    @test new_tv.transform == ft
+    @test new_logjac ≈ logjac
+
+    # Mismatched transform should error
+    strategy_bad = DynamicPPL.WithTransforms(VarNamedTuple(; x=wrong_ft), UnlinkAll())
+    @test_throws ErrorException DynamicPPL.apply_transform_strategy(
+        strategy_bad, tv, vn, dist
+    )
 end
 
 @info "Completed $(@__FILE__) in $(now() - __now__)."
