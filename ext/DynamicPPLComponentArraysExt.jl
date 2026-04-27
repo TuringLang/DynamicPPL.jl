18424f153924dc6701c87abc5cb363aa5fc317b5
Arpan Chakraborty <chakrabortyarpan151@gmail.com>
2026-04-27 22:37:38 +0530

    fix: use label2index and ComponentVector, remove make_leaf overload

--- ext/DynamicPPLComponentArraysExt.jl
+++ ext/DynamicPPLComponentArraysExt.jl
@@ -1,9 +1,15 @@
 module DynamicPPLComponentArraysExt
 
 using DynamicPPL: DynamicPPL
-using DynamicPPL.VarNamedTuples: PartialArray, AllowAll, SetPermissions, _setindex_optic!!
-using ComponentArrays: ComponentArrays, ComponentVector
+using DynamicPPL.VarNamedTuples:
+    PartialArray,
+    AllowAll,
+    SetPermissions,
+    _setindex_optic!!
+using ComponentArrays: ComponentArray, ComponentVector
 using AbstractPPL
+
+
 
 function DynamicPPL.VarNamedTuples._setindex_optic!!(
     pa::PartialArray{<:Any,<:Any,<:ComponentVector},
@@ -12,7 +18,7 @@ function DynamicPPL.VarNamedTuples._setindex_optic!!(
     template,
     permissions::SetPermissions=AllowAll(),
 ) where {S}
-    idx = ComponentArrays.label2index(pa.data, S)
+    idx = ComponentArrays.label2index(template, S)
     index_optic = AbstractPPL.Index((idx,), NamedTuple(), optic.child)
     return _setindex_optic!!(pa, value, index_optic, template, permissions)
 end
