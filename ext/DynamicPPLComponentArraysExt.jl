3f871cc0bdc48f747875cfe6846da156794cb6a9
Arpan Chakraborty <chakrabortyarpan151@gmail.com>
2026-04-27 10:00:22 +0530

    Add ComponentArraysExt to fix mixed index/property access
    Fixes #1230
    
    ComponentArray variables that mix index-based (x[1]) and
    property-based (x.a) access in the same model would crash with
    a MethodError in _setindex_optic!!.
    
    Added ext/DynamicPPLComponentArraysExt.jl with two overloads:
    - make_leaf for ComponentArray templates
    - _setindex_optic!! for PartialArray{ComponentArray} + Property optic

---
+++ ext/DynamicPPLComponentArraysExt.jl
@@ -0,0 +1,41 @@
+module DynamicPPLComponentArraysExt
+
+using DynamicPPL: DynamicPPL
+using DynamicPPL.VarNamedTuples:
+    PartialArray,
+    AllowAll,
+    SetPermissions,
+    make_leaf_singleindex,
+    make_leaf_multiindex,
+    _is_multiindex,
+    _setindex_optic!!
+using ComponentArrays: ComponentArray, getaxes
+using AbstractPPL
+
+function DynamicPPL.VarNamedTuples.make_leaf(
+    value,
+    optic::AbstractPPL.Index,
+    template::ComponentArray,
+)
+    coptic = AbstractPPL.concretize_top_level(optic, template)
+    return if _is_multiindex(template, coptic.ix...; coptic.kw...)
+        make_leaf_multiindex(value, coptic, template)
+    else
+        make_leaf_singleindex(value, coptic, template)
+    end
+end
+
+function DynamicPPL.VarNamedTuples._setindex_optic!!(
+    pa::PartialArray{<:Any,<:Any,<:ComponentArray},
+    value,
+    optic::AbstractPPL.Property{S},
+    template,
+    permissions::SetPermissions=AllowAll(),
+) where {S}
+    ax = getaxes(pa.data)[1]
+    idx = ax[S].idx
+    index_optic = AbstractPPL.Index((idx,), NamedTuple(), optic.child)
+    return _setindex_optic!!(pa, value, index_optic, template, permissions)
+end
+
+end
