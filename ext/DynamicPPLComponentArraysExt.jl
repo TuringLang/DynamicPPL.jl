9631c586b47e116afa84dc60c189200652bcde00
Arpan Chakraborty <chakrabortyarpan151@gmail.com>
2026-04-28 02:28:51 +0530

    fix: use S directly in label2index instead of String(S)

--- ext/DynamicPPLComponentArraysExt.jl
+++ ext/DynamicPPLComponentArraysExt.jl
@@ -12,7 +12,7 @@ function DynamicPPL.VarNamedTuples._setindex_optic!!(
     template,
     permissions::SetPermissions=AllowAll(),
 ) where {S}
-    idx = only(label2index(parent(template), String(S)))
+    idx = only(label2index(parent(template), S))
     index_optic = AbstractPPL.Index((idx,), NamedTuple(), optic.child)
     return _setindex_optic!!(pa, value, index_optic, template, permissions)
 end
