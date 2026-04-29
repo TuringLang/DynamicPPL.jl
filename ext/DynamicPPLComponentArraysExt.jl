159513b8a43e6af72cf62ccd44799dbe457fb5b0
Arpan Chakraborty <chakrabortyarpan151@gmail.com>
2026-04-29 07:50:12 +0530

    fix: handle Property optic in make_leaf for ComponentVector

--- ext/DynamicPPLComponentArraysExt.jl
+++ ext/DynamicPPLComponentArraysExt.jl
@@ -1,9 +1,34 @@
 module DynamicPPLComponentArraysExt
 using DynamicPPL: DynamicPPL
 using DynamicPPL.VarNamedTuples:
-    PartialArray, AllowAll, SetPermissions, _setindex_optic!!, _getindex_optic
+    PartialArray,
+    AllowAll,
+    SetPermissions,
+    _setindex_optic!!,
+    _getindex_optic,
+    make_leaf,
+    make_leaf_singleindex,
+    _is_multiindex,
+    make_leaf_multiindex
 using ComponentArrays: ComponentArrays, ComponentArray, ComponentVector
 using AbstractPPL
+
+function DynamicPPL.VarNamedTuples.make_leaf(
+    value, optic::AbstractPPL.Property{S}, template::ComponentVector
+) where {S}
+    if optic.child isa AbstractPPL.Iden
+        ax = ComponentArrays.getaxes(template)[1]
+        idx = first(ax[S].idx)
+        index_optic = AbstractPPL.Index((idx,), NamedTuple(), optic.child)
+        return make_leaf(value, index_optic, template)
+    else
+        return invoke(
+            make_leaf,
+            Tuple{Any, AbstractPPL.Property{S}, AbstractArray},
+            value, optic, template,
+        )
+    end
+end
 
 function DynamicPPL.VarNamedTuples._setindex_optic!!(
     pa::PartialArray{<:Any,<:Any,<:ComponentVector},
@@ -19,7 +44,8 @@ function DynamicPPL.VarNamedTuples._setindex_optic!!(
 end
 
 function DynamicPPL.VarNamedTuples._getindex_optic(
-    pa::PartialArray{<:Any,<:Any,<:ComponentVector}, optic::AbstractPPL.Property{S}
+    pa::PartialArray{<:Any,<:Any,<:ComponentVector},
+    optic::AbstractPPL.Property{S},
 ) where {S}
     ax = ComponentArrays.getaxes(pa.data)[1]
     idx = first(ax[S].idx)
